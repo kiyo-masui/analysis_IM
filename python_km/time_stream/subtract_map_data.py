f7b0dca27b4b32a10dd7617ac0b0ec5733b450b3
Kiyoshi Masui <kiyo@cita.utoronto.ca>
2011-01-03 16:51:58 -0500

    Did lots of work on subtracting map from data.  Can now correlate to get gain and write gain to a pickle.

--- python_km/time_stream/subtract_map_data.py
+++ python_km/time_stream/subtract_map_data.py
@@ -1,6 +1,8 @@
 """Module that subtracts map data from time_stream data, leaving only noise.
 """
 
+import cPickle
+
 import scipy as sp
 import numpy.ma as ma
 
@@ -20,9 +22,10 @@ class Subtract(base_single.BaseSingle) :
     
     prefix = 'sm_'
     params_init = {
-                   'map_file' : 'testfile_map.fits'
-                   # XXX: how to deal with time mean? (often this doesn't
-                   # matter.)
+                   'map_file' : 'testfile_map.fits',
+                   'solve_for_gain' : False,
+                   # Empty string to not write an output.
+                   'gain_output_end' : ''
                    # XXX: What about if I start subtracing off a linear piece.
                    }
 
@@ -37,13 +40,43 @@ class Subtract(base_single.BaseSingle) :
         self.Map = fits_map.read(map_file_name, 1, feedback=self.feedback)
 
     def action(self, Data) :
-        sub_map(Data, self.Map)
+        if (not self.params['solve_for_gain'] or
+            self.params['gain_output_end'] is '') :
+            sub_map(Data, self.Map, self.params['solve_for_gain'])
+        else :
+            block_gain = {}
+            Data.calc_freq()
+            block_gain['freq'] = sp.copy(Data.freq)
+            block_gain['time'] = Data.field['TIMESTAMP']
+            block_gain['scan'] = Data.field['SCAN']
+            block_gain['gain'] = sub_map(Data, self.Map, True)
+            self.gain_list.append(block_gain)
+
         Data.add_history('Subtracted map from data.', 
                          ('Map file: ' + self.params['map_file'],))
-
         return Data
 
-def sub_map(Data, Map) :
+    # Overwrite the base single process file method so we can also pickle the
+    # gains we've solved for.
+    def process_file(self, file_ind) :
+        
+        # Initialize a list to hold the gains calculated in sub_map.
+        self.gain_list = []
+        # Do the normal thing from the base class.
+        base_single.BaseSingle.process_file(self, file_ind)
+        # Pickle the gain list.
+        if (self.params['solve_for_gain'] and
+            not self.params['gain_output_end'] is '') :
+            file_middle = self.params['file_middles'][file_ind]
+            gain_fname = (self.params['output_root'] + file_middle
+                          + self.params['gain_output_end'])
+            f = open(gain_fname, 'w')
+            cPickle.dump(self.gain_list, f, 0)
+
+
+
+
+def sub_map(Data, Map, correlate=False) :
     """Subtracts a Map out of Data."""
 
     # Some dimension checks.  Eventually may want to have a tuple of maps, one
@@ -60,12 +93,10 @@ def sub_map(Data, Map) :
     Data.calc_pointing()
     Data.calc_freq()
     centre, shape, spacing = map.tools.get_map_params(Map)
-    nt_data = Data.dims[0]
-    nf_data = Data.dims[3]
     # These indices are the length of the time axis. Integer indicies.
     ra_ind = map.tools.calc_inds(Data.ra, centre[0], shape[0], spacing[0])
     dec_ind = map.tools.calc_inds(Data.dec, centre[1], shape[1], spacing[1])
-    # Length of the frequency axis.
+    # Length of the data frequency axis.
     freq_ind = map.tools.calc_inds(Data.freq, centre[2], shape[2], spacing[2])
     # Exclude indices that are off map or out of band. Boolian indices.
     on_map_inds = sp.logical_and(sp.logical_and(ra_ind>=0, ra_ind<shape[0]),
@@ -74,15 +105,36 @@ def sub_map(Data, Map) :
     # Broadcast to the same shape and combine.
     covered_inds = sp.logical_and(on_map_inds[:, sp.newaxis], 
                                   in_band_inds[sp.newaxis, :])
-    # Subtract the map from the data.  Mask the covered indicies.
+    # Make an array of map data the size of the time stream data.
     submap = Map.data[ra_ind[on_map_inds], dec_ind[on_map_inds], :]
     submap = submap[:, freq_ind[in_band_inds]]
-    for cal_ind in range(Data.dims[2]) :
-        Data.data[:,pol_ind, cal_ind, :][covered_inds] = (
-            Data.data[:,pol_ind, cal_ind, :][covered_inds] - submap.flatten())
-        Data.data[:,pol_ind, cal_ind, :][sp.logical_not(covered_inds)] = (
-            ma.masked)
-    
+    # submap is the size of the data that is on the map.  Expand to full size 
+    # of data.
+    subdata = ma.zeros(sp.shape(covered_inds))
+    subdata[covered_inds] = submap.flatten()
+    del submap
+    subdata[sp.logical_not(covered_inds)] = ma.masked
+    # If solving for gains, need a place to store them.
+    if correlate :
+        out_gains = sp.empty(Data.dims[1:4])
 
-    
-    
+    # Now start using the actual data.  Loop over cal and pol indicies.
+    for cal_ind in range(Data.dims[2]) :
+        data = Data.data[:,pol_ind, cal_ind, :]
+        # Correlate to solve for an unknown gain.
+        if correlate :
+            # Find the common mask.
+            un_mask = sp.logical_not(sp.logical_or(data.mask, subdata.mask))
+            # Subtract out the mean from both data and map.
+            tsubdata = subdata - sp.sum(un_mask*subdata, 0)/sp.sum(un_mask, 0)
+            tdata = data - sp.sum(un_mask*data, 0)/sp.sum(un_mask, 0)
+            gain = (sp.sum(un_mask*tsubdata*tdata, 0) 
+                    / sp.sum(un_mask*tsubdata*tsubdata, 0))
+            out_gains[pol_ind,cal_ind,:] = gain
+        else :
+            gain = 1.0
+
+        # Now do the subtraction and mask the off map data.
+        data -= gain*subdata
+    if correlate :
+        return out_gains
--- python_km/time_stream/test_subtract_map_data.py
+++ python_km/time_stream/test_subtract_map_data.py
@@ -2,6 +2,7 @@
 
 import unittest
 import os
+import cPickle
 
 import scipy as sp
 import numpy.ma as ma
@@ -43,12 +44,13 @@ class TestSubMap(unittest.TestCase) :
         Data.calc_freq()
         Map = self.Map
         # Set all data = (f + cal_ind)*time_ind
-        Data.data[:,:,:,:] = (sp.arange(1,11)[:,sp.newaxis,sp.newaxis,sp.newaxis]
+        Data.data[:,:,:,:] = (sp.arange(-4.5, 5)
+                              [:,sp.newaxis,sp.newaxis,sp.newaxis]
                               *(Data.freq/1e6+sp.arange(2).reshape((1,1,2,1))))
         Map.calc_axes()
         Map.data[:,:,:] = 0.0
         # Set 10 pixels to match data (except for cal_ind part).
-        Map.data[range(10), range(10), :] = (sp.arange(1,11)[:,sp.newaxis]
+        Map.data[range(10), range(10), :] = (sp.arange(-4.5, 5)[:,sp.newaxis]
                                              * Map.freq/1e6)
         # Rig the pointing to point to those 10 pixels.
         def rigged_pointing() :
@@ -56,11 +58,78 @@ class TestSubMap(unittest.TestCase) :
             Data.dec = Map.lat[range(10)]
         Data.calc_pointing = rigged_pointing
         smd.sub_map(Data, Map)
-        # Now data should be just time_ind*cal_ind, within 2.0 MHz.
-        Data.data /= sp.arange(1,11)[:,sp.newaxis,sp.newaxis,sp.newaxis]
+        # Now data should be just time_ind*cal_ind, within 2.0 MHz/2.
+        Data.data /= sp.arange(-4.5,5)[:,sp.newaxis,sp.newaxis,sp.newaxis]
         self.assertTrue(sp.allclose(Data.data[:,:,0,:], 0.0, atol=1.05))
         self.assertTrue(sp.allclose(Data.data[:,:,1,:], 1.0, atol=1.05))
 
+    def test_correlate(self) :
+        Data = self.blocks[0]
+        Data.calc_freq()
+        Map = self.Map
+        gain = 3.45
+        const = 2.14
+        # Set all data = gain*(cos(time_ind)).
+        Data.data[:,:,:,:] = gain*sp.cos(sp.arange(1,11)
+                                    [:,sp.newaxis,sp.newaxis,sp.newaxis])
+        # Explicitly set time mean to something known.
+        Data.data -= ma.mean(Data.data, 0)
+        Data.data += gain*const*Data.freq/1e6
+        # Now the Map.
+        Map.calc_axes()
+        Map.data[:,:,:] = 0.0
+        # Set 10 pixels to match cos part of data.
+        Map.data[range(10), range(10), :] = (
+                    sp.cos(sp.arange(1,11)[:,sp.newaxis]))
+        Map.data[range(10), range(10), :] -= ma.mean(Map.data[range(10), 
+                    range(10), :], 0)
+        # Give Map a mean o test things out.
+        Map.data += 2.5
+        # Rig the pointing to point to those 10 pixels.
+        def rigged_pointing() :
+            Data.ra = Map.long[range(10)]
+            Data.dec = Map.lat[range(10)]
+        Data.calc_pointing = rigged_pointing
+        smd.sub_map(Data, Map, correlate=True)
+        # Subtract the Map mean part.
+        Data.data += gain*2.5
+        # Now data should be just be gain*const*f, within machine precision.
+        Data.data /= gain*Data.freq/1e6
+        self.assertTrue(sp.allclose(Data.data[:,:,:,:], const))
+    
+    def test_masked_map(self) :
+        Data = self.blocks[0]
+        Data.calc_freq()
+        Map = self.Map
+        # Set all data = f*time_ind
+        Data.data[:,:,:,:] = (Data.freq/1e6*sp.arange(-4.5, 5)
+                              [:,sp.newaxis,sp.newaxis,sp.newaxis])
+        Data.data[3,:,:,:] = ma.masked
+        Map.calc_axes()
+        Map.data[:,:,:] = 0.0
+        # Set 10 pixels to match data.
+        Map.data[range(10), range(10), :] = (sp.arange(-4.5, 5)[:,sp.newaxis]
+                                             * Map.freq/1e6)
+        # Mask one of these pixels. Masking 4 and 5 preserves the 0 mean.
+        Map.data[5,5,:] = 99999999.
+        Map.data[5,5,:] = ma.masked
+        Map.data[4,4,:] = ma.masked
+        # Rig the pointing to point to those 10 pixels.
+        def rigged_pointing() :
+            Data.ra = Map.long[range(10)]
+            Data.dec = Map.lat[range(10)]
+        Data.calc_pointing = rigged_pointing
+        smd.sub_map(Data, Map)
+        self.assertTrue(sp.alltrue(Data.data[5,:,:,:].mask))
+        self.assertTrue(sp.alltrue(Data.data[4,:,:,:].mask))
+        self.assertTrue(sp.alltrue(Data.data[3,:,:,:].mask))
+        Data.data[5,:,:,:] = 0
+        Data.data[4,:,:,:] = 0
+        Data.data[3,:,:,:] = 0
+        # Now data should be just time_ind*cal_ind, within 2.0 MHz/2.
+        Data.data /= sp.arange(-4.5,5)[:,sp.newaxis,sp.newaxis,sp.newaxis]
+        self.assertTrue(sp.allclose(Data.data, 0.0, atol=1.05))
+   
     def test_off_map(self) :
         Data = self.blocks[0]
         Data.calc_freq()
@@ -95,7 +164,9 @@ class TestModule(unittest.TestCase) :
 
         self.params = {'sm_file_middles' : ("test",),
                        'sm_input_end' : "_rot.testout.fits",
-                       'sm_output_end' : "_sub.testout.fits"
+                       'sm_output_end' : "_sub.testout.fits",
+                       'sm_solve_for_gain' : True,
+                       'sm_gain_output_end' : '_gain.pickle'
                        }
 
     def test_history(self) :
@@ -107,6 +178,7 @@ class TestModule(unittest.TestCase) :
         os.remove('test_rot.testout.fits')
         os.remove('test_sub.testout.fits')
         os.remove('params.ini')
+        os.remove('test_gain.pickle')
         
 
 
