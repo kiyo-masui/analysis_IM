0f1be88889bfb11b84c13d0d9bff1f46551c3b01
Kiyoshi Masui <kiyo@cita.utoronto.ca>
2011-01-19 10:41:26 -0500

    Messing with folded_matrix module.

--- python_km/core/folded_matrix.py
+++ python_km/core/folded_matrix.py
@@ -17,20 +17,37 @@ class FoldedMatrix(object) :
     want to use this as a numpy array, you will have to pull either all or part
     of the data out of this object.
 
-    Note that the structure of the matrix does not have to be restricted to the
-    primary diagonal. More on this idea later.
+    The external interface of this object looks somewhat like an (n, m) array
+    where internally it is stored as (z, x, y) array with z*x=n and z*y=m. z is
+    the number of blocks and (x, y) is the shape of each block.  Compatibility
+    with these conditions is checked when creating a new instance.
+
+    Usage :
+    FoldedMatrix((n,m), (z,x,y))
+        Create a new FoldedMatrix object, allowcating new memory.
+    FoldedMatrix((n,m), (z,x,y), 'file_name')
+        Same as above but memory map it to file_name (see open).
+    FoldedMatrix((n,m), array)
+        Create a FoldedMatrix object from a numpy array.  The array must have
+        ndim=3 and have a shape compatible with (n,m).
+    Mat = open('file_name'[, mode='r+']) 
+        Open a saved memory mapped FoldedMatrix object. 
     """
 
-    def __init__(self, mat_dims, array, file_name=None) :
+    def __init__(self, array, file_name=None) :
         """See class doc string."""
         
-        if len(mat_dims) == 2 :
-            self.shape = tuple(mat_dims)
-        else :
-            raise ValueError("Argument mat_dims must be sequence of length 2")
-        if isinstance(array, sp.ndarray) and array.ndim==3 :
-            self.data = array
-        elif len(array) == 3 :
+        # If user passed an array, use that as storage.
+        if isinstance(array, sp.ndarray) :
+            if array.ndim == 3 :
+                self.data = sp.array(array, copy=False)
+            elif array.ndim == 2 :
+                self.data = sp.array(array, copy=False).reshape((1,) +
+                                                                array.shape)
+        # Otherwise allowcate memory.
+        elif len(array) == 3 or len(array) == 2 :
+            if len(array) == 2 :
+                array = (1,) + tuple(array)
             if file_name is None :
                 self.data = sp.empty(array, dtype=float)
             elif isinstance(file_name, str) :
@@ -39,6 +56,10 @@ class FoldedMatrix(object) :
             else :
                 raise TypeError("Argument 'file_name' must be a string.")
         else :
-            raise ValueError("Second argument must be either of 3D array or a "
-                             "sequence of length 3.")
-        
+            raise ValueError("Second argument must be either a 2D or 3D array" 
+                             "or a sequence of length 2 or 3.")
+        # Set the outer dimensions: the shape of the expanded block diagonal
+        # array.
+        self.shape = (self.data.shape[0]*self.data.shape[1], 
+                      self.data.shape[0]*self.data.shape[2])
+
--- python_km/core/test_folded_matrix.py
+++ python_km/core/test_folded_matrix.py
@@ -12,33 +12,35 @@ import kiyopy.custom_exceptions as ce
 class TestInit(unittest.TestCase) :
     
     def test_init_from_scratch(self) :
-        Mat = fm.FoldedMatrix((20, 40), (16, 5, 5))
-        self.assertEqual(Mat.shape, (20,40))
-        self.assertEqual(Mat.data.shape, (16, 5, 5))
+        Mat = fm.FoldedMatrix((5, 4, 8))
+        self.assertEqual(Mat.shape, (20, 40))
+        self.assertEqual(Mat.data.shape, (5, 4, 8))
 
     def test_init_memap(self) :
-        Mat = fm.FoldedMatrix((20, 40), (16, 5, 5), 'temp.npz')
+        Mat = fm.FoldedMatrix((10, 2, 4), 'temp.npz')
         self.assertTrue(isinstance(Mat.data, sp.memmap))
-        self.assertEqual(Mat.shape, (20,40))
-        self.assertEqual(Mat.data.shape, (16, 5, 5))
+        self.assertEqual(Mat.shape, (20, 40))
+        self.assertEqual(Mat.data.shape, (10, 2, 4))
         os.remove('temp.npz')
 
     def test_init_array(self) :
-        a = sp.empty((16, 5, 5))
-        Mat = fm.FoldedMatrix((20, 40), a, 'temp.npz')
+        a = sp.empty((15, 2, 3))
+        Mat = fm.FoldedMatrix(a)
         del a
-        self.assertEqual(Mat.shape, (20,40))
-        self.assertEqual(Mat.data.shape, (16, 5, 5))
+        self.assertEqual(Mat.shape, (30, 45))
+        self.assertEqual(Mat.data.shape, (15, 2, 3))
 
-    def test_demands_right_dimensions(self) :
-        self.assertRaises(ValueError, fm.FoldedMatrix, (20, 40, 1), (16, 5, 5))
-        self.assertRaises(ValueError, fm.FoldedMatrix, (20, 40), (16, 55))
-        self.assertRaises(ValueError, fm.FoldedMatrix, (20, 40),
-                          sp.ones((16,55)))
-        
-
-#class Test_
-        
+    def test_init_2D(self) :
+        # Initilize empty.
+        Mat = fm.FoldedMatrix((4, 8))
+        self.assertEqual(Mat.shape, (4, 8))
+        self.assertEqual(Mat.data.shape, (1, 4, 8))
+        # And from an array.
+        a = sp.empty((15, 7))
+        Mat = fm.FoldedMatrix(a)
+        del a
+        self.assertEqual(Mat.shape, (15, 7))
+        self.assertEqual(Mat.data.shape, (1, 15, 7))
 
 if __name__ == '__main__' :
     unittest.main()
