cb36a6cab3f322146f08473dbe99e7a9365ad253
Kiyoshi Masui <kiyo@cita.utoronto.ca>
2011-03-08 10:45:27 -0500

    Generalized combine cal for arbitrary weights.

--- python_km/time_stream/combine_cal.py
+++ python_km/time_stream/combine_cal.py
@@ -16,16 +16,19 @@ class CombineCal(base_single.BaseSingle) :
     prefix = 'cc_'
     params_init = {
                    'subtract_time_mean' : True, 
-                   'average_cal_states' : True
+                   'average_cal_states' : True,
+                   # weights should sum to 1.0 (to remain calibrated).
+                   'weights' : (0.5, 0.5)
                    }
 
     def action(self, Data):
-        combine(Data, sub_mean=self.params['subtract_time_mean'],
-                     average_cals=self.params['average_cal_states']), 
+        combine(Data, self.params["weights"], 
+                sub_mean=self.params['subtract_time_mean'],
+                average_cals=self.params['average_cal_states']), 
         Data.add_history('Combined cal-on and cal-off data.')
         return Data
 
-def combine(Data, sub_mean=True, average_cals=True) :
+def combine(Data, weights=(0.5, 0.5), sub_mean=True, average_cals=True) :
     """
     """
     
@@ -35,7 +38,8 @@ def combine(Data, sub_mean=True, average_cals=True) :
         if (Data.field['CAL'][on_ind] != 'T' or
             Data.field['CAL'][off_ind] != 'F') :
                 raise ce.DataError('Cal states not in expected order.')
-        newdata = (Data.data[:,:,[0],:] + Data.data[:,:,[1],:])/2.0
+        newdata = (Data.data[:,:,[0],:]*weights[0] +
+                   Data.data[:,:,[1],:]*weights[1])
         Data.set_data(newdata)
         Data.field['CAL'] = sp.array(['A']) # For averaged.
 
