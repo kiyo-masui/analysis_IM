8b9f4841a1814687d9ec0a00f1711d0ad009144f
Kiyoshi Masui <kiyo@cita.utoronto.ca>
2011-03-22 16:34:16 -0400

    Revert "Merge branch 'master' of github.com:kiyo-masui/analysis_IM"
    This reverts commit 579546dc402e8000f36aac7e921a7e9240a03547.
    
    Finally reversing the merge.

--- an/rfi.c
+++ an/rfi.c
@@ -1,12 +1,4 @@
 
-
-/* num_pieces = 32:  Used by the get_fit() function
-    big_number = 50: Used by get_rms()
-    threshold = 3: Used by get_rms()
-    when_to_quit = 0.1: Used by get_rms()
-    part=4:  Used by xi_square_fit() 
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -15,79 +7,77 @@
 #include "rfi.h"
 #include "Python.h"
 
+#define POINTS 2048
+#define big_number 100
+
 double absl(double x)  {  return x < 0. ? -x : x;  }
 double sq(double x)  {  return x*x;   }
+	
+void testing(double A) { printf("Hi\n"); }
+void testing2(double A, double *B) { printf("Hi\n"); }
 
-int get_rms(double *array, int len, double *rms, double *mean)    {
+void get_rms(double *array, int len, double *rms, double *mean)    {
 
 	/*  This function calculates the mean and rms of  an array of data points.
 	 array[] is the data, len is the length of the array, rms and mean are the output variables.  
 	 For a 2048 long array of XX[], you would call get_rms(XX, 2048, &rms, &mean)     */
-
-	/* Threshold = About how many sigma's is teh data treated as noise? */
-	/* when_to_quit:   Quit when successive computations of the RMS differ by this amount. */
-	   
-	double lim,rms1,count,mu,*copy,
-		   threshold = 3.,
-		   when_to_quit = 0.1;
-
-	int i,l,big_number = 50,status = 0;
 	
-	copy = (double *)(malloc(len*sizeof(double))); 
-	for(i=0; i<len; copy[i]=array[i],i++);      /* Make a copy. */
+	double lim,rms1 = 0.,count,mu;
+	int i,l;
 	
-	for(mu=0.,i=0; i<len; mu += (copy[i]/((double)(len))), i++);
-	for(i=0; i<len; copy[i] -= mu, i++);    /* subtract the mean */
-		
-	/*   A naive computation of the RMS including noise spikes.    */		
-	for(rms1=0.,i=0; i<len; rms1 += sq(copy[i]),i++);
-	rms1 = sqrt(rms1/((double)(len)));
+/*   A naive computation of the mean including noise spikes.    */		
+	mu = 0.;
+	for(i=0; i<len; i++)
+		mu += array[i];
+	mu /= (double)(len);
+/*   A similar computation of the rms.    */				
+	for(i=0; i<len; ++i)
+		rms1 += sq(array[i]-mu);
+	rms1 = sqrt(rms1/len);
 	lim = rms1; 
 	
 	for(l=0; l<big_number; l++)  {     /* never ending loop is too risky. */
 
-		/*   Recompute rms and mean ignoring suspected noise. Set your threshold. */							
-		count = 0; rms1 = 0.;		
+		count = 0; rms1 = 0.;
 		for(i=0; i<len; ++i)
-			if(sq(copy[i]) < sq(threshold*lim))
-			{  count += 1.; rms1 += sq(copy[i]);  }
+		/*   Recompute rms and mean ignoring suspected noise. Set your threshold. */					
+			if(sq(array[i]-mu) < sq(3.*lim))
+			{  count += 1.; rms1 += sq(array[i]-mu);  }
 		rms1 = sqrt(rms1/count);			
 		
-		if( absl((rms1-lim)/lim) < when_to_quit ) { status = 1; break; } /* When to exit. */
+		count = 0; mu = 0.;
+		for(i=0; i<len; ++i)
+			if(sq(array[i]) < sq(3.*lim))
+				{  count += 1.; mu += array[i];  }
+		mu /= count;
+
+		if( absl((rms1-lim)/lim) < 0.01 ) break;  /* When to exit. */
 		lim = rms1;	
 	}	
 
-	*rms = rms1; *mean = mu;
-	return status;
+	*rms = rms1;	
+	*mean = mu;
 }
 	
 void xi_square_fit(double *freq, double *array, double *fit, int len)    {
+
+#define PARTS 4    
 	
 	/*    This constructs a fit by making 'p' points by avearing over 'q' values. 
-	 I have set 'p' = 4 (see assignment 'parts=4'). 'q' is then = len/4.
-	 So,  we characterize the 128 points by means of 4 representative points. A straight line is fit through these 4 points by the least-squares
-	 method, and stored in the array 'fit'.    
-	 
-	 A power law may be better, and I will try that later. For now, it's linear.
-	 
 	 freq is the frequency array.
 	 array is the data array.
 	 fit is the output array containing the fit.
-	 len is the length..
 	 */     
 	
-	double *T,*nu,count,rms,d_parts,d_pieces,mean,
-	avg,x,y,x_bar,y_bar,sum_x_x,sum_x_y,a,b;
-	
+	double *T,*nu,count,d_parts,d_pieces,min,offset,rms,mean,
+		   x_bar,y_bar,sum_x_x,sum_x_y,a,b;	
 	int i,j,pieces,parts;
 	
 	for(i=0; i<len; fit[i]=0,++i);
 	
-	parts = 4;	
-	d_parts = (double)(parts);
-	pieces = len/parts;            
-	d_pieces = (double)(pieces);
-	
+	parts = (int)(PARTS); d_parts = (double)(PARTS);
+	pieces = len/parts; d_pieces = (double)(pieces);
+
 	T = (double *)(malloc(sizeof(double)*parts));
 	nu = (double *)(malloc(sizeof(double)*parts));
 	
@@ -95,73 +85,70 @@ void xi_square_fit(double *freq, double *array, double *fit, int len)    {
 	
 	for(i=0; i<parts; i++)   {
 		
-		nu[i] = 0.;
-		T[i] = 0.;
-		count = 0.;
+		nu[i] = 0.; T[i] = 0.; count = 0.;
 		for(j=0; j<pieces; j++)  {
 			
-			if(absl(array[j + (i*pieces)]-mean) > 5.*rms) continue;			
+			if(absl(array[j + (i*pieces)]-mean) > 5.*rms) continue;	 /* your noise threshold here. */
 			nu[i] +=  (freq[j + (i*pieces)]);
 			T[i]  += (array[j + (i*pieces)]);
 			count += 1.;
 		}
-		nu[i] /= count;
-		T[i] /= count;
+		nu[i] /= count; T[i] /= count;
 	}
 	
 	x_bar = 0.;  y_bar = 0.;  sum_x_x = 0.; sum_x_y = 0.;
-	for(i=0; i<parts; i++)    {
-		
-		x = nu[i]; y = T[i];		
-		x_bar += (x/d_parts);y_bar += (y/d_parts);		
-		sum_x_x += (x*x); sum_x_y += (x*y);		
+	for(i=0; i<parts; i++)    {     /* Fit a straight line */		
+
+		x_bar += (nu[i]/d_parts); y_bar += (T[i]/d_parts);		
+		sum_x_x += (nu[i]*nu[i]); sum_x_y += (nu[i]*T[i]);				
 	}
 	
 	b = (sum_x_y - (d_parts*x_bar*y_bar)) / (sum_x_x - (d_parts*sq(x_bar)));
 	a = y_bar - (b*x_bar);
-		
-	for(i=0; i<len; fit[i] = a + b*freq[i],i++);	
+	for(i=0; i<len; (fit[i] = a + b*freq[i]), i++);	
 }
 
-
-void get_fit(int len, double *array, double *f, double *fit_array)    {
+void get_fit(double array[POINTS], double f[POINTS], double fit_array[POINTS])    {
 	
 	/*         This flattens the waveform stored in variable array[], by means of the fit. 
 		  The fit is obtained and then removed from the data. */
 	
-	int num_pieces = 32, 
-		i,j,block = len/num_pieces;
+	/* Change  NUM_PIECES and BLOCK as required. NUM_PIECES * BLOCK = 2048 */
+	 
+	#define  NUM_PIECES 32         /* Divide into 32 parts.  */
+	#define  BLOCK 64              /* 2048/32 = 64 */
 	
-	double *pieces, *f_pieces, *fit,rms,mean;
+	int num_pieces = (int)(NUM_PIECES), block = (int)(BLOCK),i,j;
+	double pieces[NUM_PIECES][BLOCK], f_pieces[NUM_PIECES][BLOCK], fit[BLOCK],rms,mean, temp_fit[POINTS];
+	
+	for(i=0; i<num_pieces; i++)
+		for(j=0; j<block; j++)    {
+			
+			pieces[i][j] = array[j + (i*block)];
+			f_pieces[i][j] = f[j + (i*block)];
+		}	
 
-	pieces   = (double *)(malloc(sizeof(double)*block));
-	f_pieces = (double *)(malloc(sizeof(double)*block));
-	fit = (double *)(malloc(sizeof(double)*block));
-	
 	for(i=0; i<num_pieces; i++)   {
 		
-		for(j=0; j<block; j++)    
-			{  pieces[j] = array[j + (i*block)]; f_pieces[j] = f[j + (i*block)];  }
-					
-		xi_square_fit(f_pieces,pieces, fit, block);
+		xi_square_fit(f_pieces[i], pieces[i], fit, block);
 		for(j=0; j<block; fit_array[j+(block*i)] = fit[j],j++);			
 	}
+	
 }
 
 
-void flatten(double *array, double *fit_array, int len)    {
+void flatten(double array[POINTS], double fit_array[POINTS])    {
 
 	int i;
 	double rms,mean;
 	
-	for(i=0; i<len; array[i] -= fit_array[i],i++);
-	get_rms(array,len,&rms,&mean);
-	for(i=0; i<len; (array[i] -= mean), i++);		
+	for(i=0; i<POINTS; array[i] -= fit_array[i],i++);
+	get_rms(array,POINTS,&rms,&mean);
+	for(i=0; i<POINTS; (array[i] -= mean), i++);		
 }
 
-
-
-void mask_array(int size, double *array, double sig, int *mask, int tol)    {
+	
+void mask_array(int size, double array[POINTS], double sig, int mask[POINTS], int tol)    {
 
 	/* Prepare a mask. 1 mean noise, 0 means clean. 
 	 Flag all points above a threshold, and also nearby bins. */
@@ -176,7 +163,7 @@ void mask_array(int size, double *array, double sig, int *mask, int tol)    {
 }
 
 
-void rfi_find_dTdf(double *nu, double *arr, int *mask, int count, int lim)    {
+void rfi_find_dTdf(double nu[POINTS], double arr[POINTS], int mask[POINTS], int count, int lim)    {
 
 	/*   Get the noisiest XX or YY frequencies, which may have been missed. */
 	
@@ -207,10 +194,9 @@ void rfi_find_dTdf(double *nu, double *arr, int *mask, int count, int lim)    {
 		mask[i] = ( max_sign[i] > 0 ? max_pos[i]+1 : max_pos[i] );	
 }
 
-void hi_f_spikes(int len, double *array, double *f, int lim, int count, int tol)      {
+void hi_f_spikes(double array[POINTS], double f[POINTS], int lim, int count, int tol)      {
 	
-	int i,j,
-		*temp_mask = (int *)(malloc(sizeof(int)*len));
+	int i,j,temp_mask[POINTS];
 	
 	for(i=0; i<lim; temp_mask[i]=0,i++);
 	rfi_find_dTdf(f,array,temp_mask,count,lim);	
@@ -218,46 +204,38 @@ void hi_f_spikes(int len, double *array, double *f, int lim, int count, int tol)
 		for(j=temp_mask[i]-tol; j <= temp_mask[i]+tol; array[j++] = 0.);
 }
 
-void rfi_flag(int len,  int flat, int spike, int tol, double sig, double *fit, double *array, double *f, int *mask)  {
+void rfi_flag(int flat, int spike, int tol, double sig, double fit[POINTS], double array[POINTS], double f[POINTS], int mask[POINTS])  {
 
 	int i;
 	double rms,mean;
 
-	if(flat)   flatten(array,fit,len);  /* Compensate for structure in the cross correlation  */				
-	for(i=0; i<len; mask[i++]=0);				
-	get_rms(array,len,&rms,&mean);		
-	mask_array(len,array,sig*rms,mask,tol);	
+	if(flat)   flatten(array,fit);  /* Compensate for structure in the cross correlation  */				
+	for(i=0; i<POINTS; mask[i++]=0);				
+	get_rms(array,POINTS,&rms,&mean);		
+	mask_array(POINTS,array,sig*rms,mask,tol);	
 }	
 	
 
-void clean(int len, double sig, int tol, int flat, int spike, int dTdf_limit, int dTdf_tol, double *fit, double *cross1, double *array1, double *f1, int *m)  { 
+void clean(double sig, int tol, int flat, int spike, int dTdf_limit, int dTdf_tol, double fit[POINTS], double cross1[POINTS], double array1[POINTS], double f1[POINTS], int m[POINTS])  { 
 
-	int i,lim,ct,num_entries,	
-		*ind = (int *)(malloc(sizeof(int)*len)),
-		*mask = (int *)(malloc(sizeof(int)*len)),
-		*temp_ind = (int *)(malloc(sizeof(int)*len));
+	int i,ind[POINTS],lim,ct,mask[POINTS],num_entries,temp_ind[POINTS];
+	double temp_f[POINTS], temp_arr[POINTS],
+	       cross[POINTS], array[POINTS], f[POINTS];
+		
+	for(i=0; i<POINTS; ind[i]=i, i++);
+	for(i=0; i<POINTS; (cross[i]=cross1[i],array[i]=array1[i],f[i]=f1[i]),i++); /* Make a copy. */
 	
-	double 		
-		*temp_f = (double *)(malloc(sizeof(double)*len)),
-		*temp_arr = (double *)(malloc(sizeof(double)*len)),
-		*cross = (double *)(malloc(sizeof(double)*len)),
-		*array = (double *)(malloc(sizeof(double)*len)),	
-		*f = (double *)(malloc(sizeof(double)*len));
-	
-	for(i=0; i<len; ind[i]=i, i++);
-	for(i=0; i<len; (cross[i]=cross1[i],array[i]=array1[i],f[i]=f1[i]),i++); /* Make a copy. */
-	
-	rfi_flag(len,flat,spike,tol,sig,fit,cross,f,mask);		
+	rfi_flag(flat,spike,tol,sig,fit,cross,f,mask);		
 	ct=0;
-	for(i=0; i<len; i++)
+	for(i=0; i<POINTS; i++)
 		if(mask[i] == 0)            /* Collect unflagged data only. */
 		{   temp_f[ct] = f[i]; temp_arr[ct] = array[i]; temp_ind[ct] = ind[i]; ++ct; }
 	
 	if(spike) /* Do you want a further check of the data? Is so, compute dT/df. */
-		hi_f_spikes(len,temp_arr,temp_f,dTdf_limit,ct,dTdf_tol);	
+		hi_f_spikes(temp_arr,temp_f,dTdf_limit,ct,dTdf_tol);	
 
-	for(i=0; i<len; m[i]=1,i++);	/* Initialize: All points noisy. */
-	for(i=0; i<len; (f[i]=0.,array[i]=0.,ind[i]=0),i++);			
+	for(i=0; i<POINTS;m[i]=1,i++);	/* Initialize: All points noisy. */
+	for(i=0; i<POINTS; (f[i]=0.,array[i]=0.,ind[i]=0),i++);			
 	num_entries=0;
 	for(i=0; i<ct; i++)
 		if(temp_arr[i]!=0.) 
--- an/rfi.h
+++ an/rfi.h
@@ -1,13 +1,14 @@
 
 extern double absl(double);
 extern double sq(double);
-extern int get_rms(double *, int, double *, double *);
-extern void xi_square_fit(double *, double *, double *, int);
-extern void get_fit(int, double *, double *, double *);
-extern void flatten(double *, double *, int);
-extern void mask_array(int, double *, double, int *, int);
-extern void rfi_find_dTdf(double *, double *, int *, int, int);
-extern void hi_f_spikes(int, double *, double *, int, int, int);
-extern void rfi_flag(int, int, int, int, double, double *, double *, double *, int *);
-extern void clean(int, double, int, int, int, int, int, double *, double *, double *, double *, int *);
-
+extern void testing(double A);
+extern void testing2(double A, double B[10]);
+extern void get_rms(double A[2048], int, double B[2048], double C[2048]);
+extern void xi_square_fit(double A[2048], double B[2048], double C[2048], int);
+extern void get_fit(double A[2048], double B[2048], double C[2048]);
+extern void flatten(double A[2048], double B[2048]);
+extern void mask_array(int, double A[2048], double, int B[2048], int);
+extern void rfi_find_dTdf(double A[2048], double B[2048], int C[2048], int, int);
+extern void hi_f_spikes(double A[2048], double B[2048], int, int, int);
+extern void rfi_flag(int, int, int, double, double A[2048], double B[2048], double C[2048], int D[2048]);
+extern void clean(double, int, int, int, int, int, double A[2048], double B[2048], double C[2048], double D[2048], int E[2048]);
