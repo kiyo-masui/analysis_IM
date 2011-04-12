891505885cdfe5a112194c2e7867b91a133315a9
Tabitha Voytek <tcv@chime.cita.utoronto.ca>
2011-04-12 09:58:17 -0400

    Most recent updates for mueller generation system.

--- python_km/time_stream/pol_cal.py
+++ python_km/time_stream/pol_cal.py
@@ -3,6 +3,7 @@
 import scipy as sp
 import numpy.ma as ma
 import pylab as pl
+import numpy as np
 
 import kiyopy.custom_exceptions as ce
 import base_single
@@ -80,52 +81,59 @@ class Calibrate(base_single.BaseSingle) :
 
 def mueller() :
 # Mueller Matrix parameters from polcal calculations
-    deltaG = [-0.185,-0.113,-0.213,-0.295,-0.394,-0.012,0.546,0.680]
-    psi = [-21.9,179.9,-175.9,178.9,171.2,152.5,159.9,122.5]
-    alpha = [-1.2,3.5,2.9,-1.3,10.0,-6.2,-2.2,-7.5]
-    epsilon = [0.008,0.013,0.015,0.019,0.024,0.016,0.016,0.017]
-    phi = [-164.9,-4.0,-7.9,0.5,6.1,23.2,17.6,55.4]
-    CFR = [694,724,754,784,814,844,874,904]
+#    deltaG = [-0.185,-0.113,-0.213,-0.295,-0.394,-0.012,0.546,0.680]
+#    psi = [-21.9,179.9,-175.9,178.9,171.2,152.5,159.9,122.5]
+#    alpha = [-1.2,3.5,2.9,-1.3,10.0,-6.2,-2.2,-7.5]
+#    epsilon = [0.008,0.013,0.015,0.019,0.024,0.016,0.016,0.017]
+#    phi = [-164.9,-4.0,-7.9,0.5,6.1,23.2,17.6,55.4]
+#    CFR = [694,724,754,784,814,844,874,904]
 #    delf = sp.array([(CFR[i]-1485.8) for i in range(0,8)])
 #    psi = sp.array([(psi[i]-0.0086*delf[i]) for i in range(0,8)])
-    mp = sp.array([deltaG,psi,alpha,epsilon,phi,CFR])
+#    mp = sp.array([deltaG,psi,alpha,epsilon,phi,CFR])
 
-# Matrix for rotating Mueller Matrix into Astronomical frame
-    m_astron = sp.zeros((4,4,8), float)
-    for i in range(0,8):
-        m_astron[0,0,i] = 1
-        m_astron[3,3,i] = 1
-        m_astron[1,1,i] = -1
-        m_astron[2,2,i] = -1
+
+     mp = np.loadtxt('mueller_params_calc.txt')
+#     print mp
+#This is a file with first index being freq, second index being parameter where the values are:
+# 0 = Freq, 1 = deltaG, 2 = alpha, 3 = psi, 4 = phi, 5 = epsilon, 6 = Q_src, 7 = U_src
+
+# Matrix for rotating Mueller Matrix into Astronomical frame - assumes parallactic angle = 0
+#    m_astron = sp.zeros((4,4,8), float)
+#    for i in range(0,8):
+#        m_astron[0,0,i] = 1
+#        m_astron[3,3,i] = 1
+#        m_astron[1,1,i] = -1
+#        m_astron[2,2,i] = -1
 
 # Generates Inverse Mueller Matrix for use
-    m_total = sp.zeros((4,4,8), float)
-    for i in range(0,8):
-#Note that this version is based on the combined matrix in the theory paper.
+     m_total = sp.zeros((4,4,260),float)
+     for i in range(0,260):
+#Note that this version is based on the combined matrix I derived corrected for linear feed:
         m_total[0,0,i] = 1
-        m_total[0,1,i] = -2*mp[3,i]*ma.sin(mp[4,i]*sp.pi/180)*ma.sin(2*mp[2,i]*sp.pi/180)+mp[0,i]*ma.cos(2*mp[2,i]*sp.pi/180)/2
-        m_total[0,2,i] = 2*mp[3,i]*ma.cos(mp[4,i]*sp.pi/180)
-        m_total[0,3,i] = 2*mp[3,i]*ma.sin(mp[4,i]*sp.pi/180)*ma.cos(2*mp[2,i]*sp.pi/180)+mp[0,i]*ma.sin(2*mp[2,i]*sp.pi/180)/2
-        m_total[1,0,i] = mp[0,i]/2
-        m_total[1,1,i] = ma.cos(2*mp[2,i]*sp.pi/180)
-        m_total[1,3,i] = ma.sin(2*mp[2,i]*sp.pi/180)
-        m_total[2,0,i] = 2*mp[3,i]*ma.cos((mp[4,i]+mp[1,i])*sp.pi/180)
-        m_total[2,1,i] = ma.sin(2*mp[2,i]*sp.pi/180)*ma.sin(mp[1,i]*sp.pi/180)
-        m_total[2,2,i] = ma.cos(mp[1,i]*sp.pi/180)
-        m_total[2,3,i] = -ma.cos(2*mp[2,i]*sp.pi/180)*ma.sin(mp[1,i]*sp.pi/180)
-        m_total[3,0,i] = 2*mp[3,i]*ma.sin((mp[4,i]+mp[1,i])*sp.pi/180)
-        m_total[3,1,i] = -ma.sin(2*mp[2,i]*sp.pi/180)*ma.cos(mp[1,i]*sp.pi/180)
-        m_total[3,2,i] = ma.sin(mp[1,i]*sp.pi/180)
-        m_total[3,3,i] = ma.cos(2*mp[2,i]*sp.pi/180)*ma.cos(mp[1,i]*sp.pi/180)
-        M_tot = sp.mat(m_total[:,:,i])
-        M_total = M_tot.I
-        M_astron = sp.mat(m_astron[:,:,i])
-        M_total = M_astron*M_total
+        m_total[0,1,i] = -2*mp[i,5]*ma.sin(2*mp[i,2]*sp.pi/180)*ma.cos(mp[i,4]*sp.pi/180)+mp[i,1]*ma.cos(2*mp[i,2]*sp.pi/180)/2
+        m_total[0,2,i] = 2*mp[i,5]*ma.cos(mp[i,4]*sp.pi/180)*ma.cos(2*mp[i,2]*sp.pi/180)+mp[i,1]*ma.sin(2*mp[i,2]*sp.pi/180)/2
+        m_total[0,3,i] = 2*mp[i,5]*ma.sin(mp[i,4]*sp.pi/180)
+        m_total[1,0,i] = mp[i,1]/2
+        m_total[1,1,i] = ma.cos(2*mp[i,2]*sp.pi/180)
+        m_total[1,2,i] = ma.sin(2*mp[i,2]*sp.pi/180)
+        m_total[2,0,i] = 2*mp[i,5]*ma.cos((mp[i,3]+mp[i,4])*sp.pi/180)
+        m_total[2,1,i] = -ma.sin(2*mp[i,2]*sp.pi/180)*ma.cos(mp[i,3]*sp.pi/180)
+        m_total[2,2,i] = ma.cos(mp[i,3]*sp.pi/180)*ma.cos(2*mp[i,2]*sp.pi/180)
+        m_total[2,3,i] = -ma.sin(mp[i,3]*sp.pi/180)
+        m_total[3,0,i] = 2*mp[i,5]*ma.sin((mp[i,3]+mp[i,4])*sp.pi/180)
+        m_total[3,1,i] = -ma.sin(2*mp[i,2]*sp.pi/180)*ma.sin(mp[i,3]*sp.pi/180)
+        m_total[3,2,i] = ma.sin(mp[i,3]*sp.pi/180)*ma.cos(2*mp[i,2]*sp.pi/180)
+        m_total[3,3,i] = ma.cos(mp[i,3]*sp.pi/180)
+        M_total = sp.mat(m_total[:,:,i])
+#        print M_total
+        M_total = M_total.I
+#        M_astron = sp.mat(m_astron[:,:,i])
+#        M_total = M_astron*M_total
         for j in range(0,4):
             for k in range(0,4):
                 m_total[j,k,i] = M_total[j,k]
-
-    return m_total
+#     print m_total[:,:,79]
+     return m_total
 
 
 def calibrate_pol(Data, m_total) :
@@ -161,42 +169,69 @@ def calibrate_pol(Data, m_total) :
     Data.calc_freq()
     # Now data has an atribute Data.freq which is an array that gives the
     # frequency along the last axis.
-     
+          
     # Data.field['CRVAL1'] is center frequency in Hz. 
     # Data.data 4 dim array 2nd index polarization, 4th index frequency. 
 
+    # Need to get parallactic angle:
+    Data.calc_PA()
+    # This gives an array (Data.PA) of PA values of length = time dim.
+
     for time_index in range(0,Data.dims[0]):
+
+    #Generate a sky matrix for this time index:
+        m_sky = sp.zeros((4,4))
+        m_sky[0,0] = 1
+        m_sky[1,1] = ma.cos(2*Data.PA[time_index]*sp.pi/180)
+        m_sky[1,2] = ma.sin(2*Data.PA[time_index]*sp.pi/180)
+        m_sky[2,1] = -ma.sin(2*Data.PA[time_index]*sp.pi/180)
+        m_sky[2,2] = ma.cos(2*Data.PA[time_index]*sp.pi/180)
+        m_sky[3,3] = 1
+
+        M_sky = sp.mat(m_sky)
+        M_sky = M_sky.I
+#        print M_sky
+
         for cal_index in range(0,Data.dims[2]):
         # Determines the Inverse Mueller Matrix to use   
 #            CenterFrequency = int(Data.field['CRVAL1']/1000000)
             for freq in range(0,Data.dims[3]):
-               if Data.freq[freq] in range(669,699):
-                  bin = 0
-               elif Data.freq[freq] in range(700,729):
-                  bin = 1
-               elif Data.freq[freq] in range(730,759):
-                  bin = 2
-               elif Data.freq[freq] in range(760,789):
-                  bin = 3
-               elif Data.freq[freq] in range(790,819):
-                  bin = 4
-               elif Data.freq[freq] in range(820,849):
-                  bin = 5
-               elif Data.freq[freq] in range(850,879):
-                  bin = 6
-               elif Data.freq[freq] in range(880,929):
-                  bin = 7
-               else :
-                  raise ce.DataError('The frequency outside viable window') 
+#               if Data.freq[freq] in range(669,699):
+#                  bin = 0
+#               elif Data.freq[freq] in range(700,729):
+#                  bin = 1
+#               elif Data.freq[freq] in range(730,759):
+#                  bin = 2
+#               elif Data.freq[freq] in range(760,789):
+#                  bin = 3
+#               elif Data.freq[freq] in range(790,819):
+#                  bin = 4
+#               elif Data.freq[freq] in range(820,849):
+#                  bin = 5
+#               elif Data.freq[freq] in range(850,879):
+#                  bin = 6
+#               elif Data.freq[freq] in range(880,929):
+#                  bin = 7
+#               else :
+#                  raise ce.DataError('The frequency outside viable window') 
 
+     # Tells which mueller matrix to use.
+               frequency = int(Data.freq[freq]/1000000) 
+               bin = 929-frequency
+#               print bin
     # Converts files into matrix format 
-               STOKES = sp.mat(Data.data[time_index,:,cal_index,freq])        
+               STOKES = Data.data[time_index,:,cal_index,freq]       
+#               print STOKES
                MUELLER = sp.mat(m_total[:,:,bin])
+#               print MUELLER
+
     # Next there is a matrix multiplication that will generate 
     # a new set of stokes values.
-               stokesmod = MUELLER*STOKES
+               stokesmod = np.dot(MUELLER,STOKES)
+               stokesmod = np.dot(M_sky,stokesmod)
+#               print stokesmod
                for i in range(0,Data.dims[1]):
-                    Data.data[time_index,i,cal_index,j] = stokesmod[i]	
+                    Data.data[time_index,i,cal_index,freq] = stokesmod[i]	
 
     # At this point the polarization values should be adjusted. 
 
