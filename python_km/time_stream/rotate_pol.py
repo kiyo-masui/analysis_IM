cc791cae0434a66ec9e3ea744ba356bc2385e0ef
Kiyoshi Masui <kiyo@cita.utoronto.ca>
2011-05-06 09:10:54 -0400

    Fixed a factor of 2 in rotate_pol.

--- python_km/time_stream/rotate_pol.py
+++ python_km/time_stream/rotate_pol.py
@@ -59,11 +59,11 @@ def rotate(Data, new_pols=(1,), average_cals=False) :
         elif tuple(new_pols) == (-5, -7, -8, -6) :
             new_data = ma.empty(Data.dims)
             new_data[:,[0],:,:] = (Data.data[:,[Q_ind],:,:] 
-                                   + Data.data[:,[I_ind],:,:])/2.0
+                                   + Data.data[:,[I_ind],:,:])
             new_data[:,[1],:,:] = Data.data[:,[U_ind],:,:] 
             new_data[:,[2],:,:] = Data.data[:,[V_ind],:,:] 
             new_data[:,[3],:,:] = (Data.data[:,[I_ind],:,:] 
-                                   - Data.data[:,[Q_ind],:,:])/2.0
+                                   - Data.data[:,[Q_ind],:,:])
         else :
             msg = ("Converstion to " + str(tuple(new_pols)) + " from " 
                    + str(tuple(Data.field['CRVAL4'])) + " is not supported.")
