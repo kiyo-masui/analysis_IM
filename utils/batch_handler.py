ac8170bea123d9e9933dd728ed15b1be7608b12f
Eric Switzer <eric.switzer@gmail.com>
2011-12-21 12:23:44 -0500

    batch handler cleaned up and documented

--- utils/batch_handler.py
+++ utils/batch_handler.py
@@ -1,34 +1,15 @@
+"""manage batch runs"""
 import multiprocessing
 import cPickle as pickle
 import hashlib
-import numpy as np
 import re
 import shelve
-import sys
-
-# TODO: incorporate class persistence to have this save itself in the directory
-# save itself using presistent class (selected variables)
-# list directory -> list of hashes known?
-# using persist .load to reconstruct class and make hash -> variables list
-# function: regenerate regardless of whether hash exists
-# initialization: generation mode or consumer mode
-# generation mode: always return md5; only add to stack if not known
-# consumer mode: raise exception if not known, return result if known
-# TODO: write PBS caller
 
 
-def function_wrapper(args):
-    r"""this wrapper supports MemoizeBatch and circumvents the fact that
-    multiprocessing pool's map can not handle class functions or generic
-    function arguments to calls in that pool. Note that we also save the data
-    here as opposed to handing it back to multiprocessing. This is to prevent
-    the outputs from a potentially large number of function calls from being
-    saved in memory.
-    """
-    (hash, directory, funcname, args, kwargs) = args
-
-    funcattr = None
-    funcsplit = funcname.split(".")
+def print_call(args):
+    r"""Print the function and its arguments and the file in which the outputs
+    are saved in the cache directory."""
+    (signature, directory, funcname, args, kwargs) = args
 
     kwlist = []
     for item in kwargs:
@@ -42,11 +23,29 @@ def function_wrapper(args):
 
     argstring = ", ".join(arglist)
 
-    filename = "%s/%s.shelve" % (directory, hash)
+    filename = "%s/%s.shelve" % (directory, signature)
     filename = re.sub('/+', '/', filename)
 
     # TODO: sometimes there are spurious commas
     print "%s(%s, %s) -> %s" % (funcname, argstring, kwstring, filename)
+
+    return filename
+
+
+def function_wrapper(args_package):
+    r"""this wrapper supports MemoizeBatch and circumvents the fact that
+    multiprocessing pool's map can not handle class functions or generic
+    function arguments to calls in that pool. Note that we also save the data
+    here as opposed to handing it back to multiprocessing. This is to prevent
+    the outputs from a potentially large number of function calls from being
+    saved in memory.
+    """
+    (signature, directory, funcname, args, kwargs) = args_package
+
+    filename = print_call(args_package)
+
+    funcattr = None
+    funcsplit = funcname.split(".")
 
     if len(funcsplit) > 1:
         mod = __import__(".".join(funcsplit[0:-1]))
@@ -58,11 +57,9 @@ def function_wrapper(args):
         funcattr = globals()[funcsplit[0]]
 
     result = funcattr(*args, **kwargs)
-    print result
-    # TODO now save the outputs
 
     outfile = shelve.open(filename, 'n')
-    outfile["hash"] = hash
+    outfile["signature"] = signature
     outfile["filename"] = filename
     outfile["funcname"] = funcname
     outfile["args"] = args
@@ -70,19 +67,60 @@ def function_wrapper(args):
     outfile["result"] = result
     outfile.close()
 
-    return hash
+    return signature
 
 
 class MemoizeBatch(object):
-    r"""
-    initialize a class that points to the function you want to call
-    call execute of that class
-       -if calculated, fetch value based on hash
-       -if not: put the parameters on a heap and assign a hash
+    r"""A class to manage a large batch of function calls and outputs.
 
-    call the parallel exec: either multiproc or PBS
+    When to use this: you have a function that runs slowly and would like to
+    run it many times, trivially parallelized over many parameters. A common
+    problem is that one code typically does all the loops for the batch
+    processing -> dump to files, and the a second code reads the files to make
+    a final product. There is overhead in naming the files and also in the fact
+    that the batch caller loops are typically quite different from the loops
+    over parameters in the final consumer.
 
-    use the same expressions to recall the data
+    This class circumvents these issues by assigning an md5 hash to the
+    arguments and writing a file by that name. If those arguments have been
+    alled, it can retrieve the output. Note that these have different md5
+    signatures for func(arg=True): call func(arg=True) and call func()
+    (even though the result will be the same.)
+
+    To specify the batch of parameters to run over, initialize the class with
+    `generate=True`. Then when execute is called, it stacks up the list of
+    parameters in the call in `self.call_stack`. To perform the computation,
+    call one of the call_stack handlers (either multiprocess_stack or
+    pbsprocess_stack) to parallelize the function call.
+
+    To use the cached values, initialize the class with `generate=False`
+    (default) and then call execute in the same way as above, and the cached
+    results will be returned by looking up the hash for the arguments.
+
+    Example to generate the cache table:
+    >>> caller1 = MemoizeBatch("trial_function", "./testdata/", generate=True)
+    >>> caller1.execute("ok",3, arg1=True, arg2=3)
+    '1d6e885be47e6f350befb10fd4b28318'
+    >>> import numpy as np
+    >>> bins = np.arange(0,1, 0.2)
+    >>> caller1.execute(5, "ok2", arg1="stringy", arg2=bins)
+    'fc3eb86900a1f325fb3369d7f686d475'
+    >>> caller1.execute("ok2",4)
+    '5b81c329717a428261f3bebe2b4b5bba'
+    >>> caller1.multiprocess_stack()
+    ['1d6e885be47e6f350befb10fd4b28318',
+     'fc3eb86900a1f325fb3369d7f686d475',
+     '5b81c329717a428261f3bebe2b4b5bba']
+
+    Example to use the cache in later computation:
+    >>> caller2 = MemoizeBatch("trial_function", "./testdata/")
+    >>> caller2.execute("ok",3, arg1=True, arg2=3)
+    ('ok', 3, True, 3)
+    >>> caller2.execute(5, "ok2", arg1="stringy", arg2=bins)
+    (5, 'ok2', 'stringy', array([ 0. ,  0.2,  0.4,  0.6,  0.8]))
+    >>> caller2.execute("ok2",4)
+    ('ok2', 4, 'e', 'r')
+
     """
     def __init__(self, funcname, directory, generate=False, regenerate=False):
         r"""
@@ -103,8 +141,6 @@ class MemoizeBatch(object):
         self.regenerate = regenerate
 
     def execute(self, *args, **kwargs):
-        r"""
-        """
         argpkl = pickle.dumps((self.funcname, args,
                                tuple(sorted(kwargs.items()))), -1)
         arghash = hashlib.md5(argpkl).hexdigest()
@@ -117,25 +153,19 @@ class MemoizeBatch(object):
 
             retval = arghash
         else:
-            #TODO: raise NotCalculated
+            # TODO: raise NotCalculated?
+            # TODO: check args, kwargs with requested?
             filename = "%s/%s.shelve" % (self.directory, arghash)
             input_shelve = shelve.open(filename, "r")
-            # check the values for sanity's sake (can prob. remove)
-            if ((input_shelve['hash'] != arghash) or
-               (input_shelve['funcname'] != self.funcname) or
-               (input_shelve['args'] != args) or
-               (input_shelve['kwargs'] != kwargs)):
-                print "ERROR: cache file is corrupted"
-                sys.exit()
-
             retval = input_shelve['result']
             input_shelve.close()
 
         return retval
 
-
     def multiprocess_stack(self, save_cpu=4):
-        r"""save_cpu is the number of CPUs to leave free
+        r"""process the call stack built up by 'execute' calls using
+        multiprocessing.
+        `save_cpu` is the number of CPUs to leave free
         """
         num_cpus = multiprocessing.cpu_count() - save_cpu
         pool = multiprocessing.Pool(processes=num_cpus)
@@ -144,36 +174,27 @@ class MemoizeBatch(object):
         print result
 
     def pbsprocess_stack(self):
+        r"""process the call stack built up by 'execute' call using the PBS
+        batch processing protocol
+        """
         print "PBS batch not implemented yet"
 
+    def report_cache_table(self):
+        r"""read through the cache directory and make a report of the arguments
+        of function for which its results are cached.
+        """
+        print "report cache is not implemented yet"
 
-def basic_function(thing1, thing2, arg1="e", arg2="r"):
+
+def trial_function(thing1, thing2, arg1="e", arg2="r"):
+    r"""a test function (won't go in docstring"""
     return thing1, thing2, arg1, arg2
 
 
 if __name__ == "__main__":
     import doctest
 
-    caller1 = MemoizeBatch("basic_function", "./testdata/", generate=True)
-    caller1.execute("ok",3, arg1=True, arg2=3)
-    bins = np.arange(0,1, 0.2)
-    caller1.execute("ok2",4, arg1="weee", arg2=bins)
-    caller1.execute("ok2",4)
-    caller1.multiprocess_stack()
-
-    print "Now testing a function from another class -----------"
-    caller2 = MemoizeBatch("utils.data_paths.extract_split_tag", "./testdata/",
-                           generate=True)
-    caller2.execute(['A;ok', 'B;ok'], divider=';')
-    caller2.execute(['B;oke', 'C;okd'], divider=';')
-    caller2.multiprocess_stack()
-
-    print "Now calling a new run -----------"
-    caller3 = MemoizeBatch("utils.data_paths.extract_split_tag", "./testdata/")
-    ret1 = caller3.execute(['A;ok', 'B;ok'], divider=';')
-    ret2 = caller3.execute(['B;oke', 'C;okd'], divider=';')
-    print "wow", ret1
-    print "wow", ret2
-
-    #print bins
-    #wrapper_function("ok", "ok2", arg1="that", arg2=bins)
+    # run some tests
+    OPTIONFLAGS = (doctest.ELLIPSIS |
+                   doctest.NORMALIZE_WHITESPACE)
+    doctest.testmod(optionflags=OPTIONFLAGS)
