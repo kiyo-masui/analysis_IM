several changes that may need to be reverted map_pair and physical_gridding: output numpy + info rather than kiyo array for pickling pwrspec_estimation.py: ddof=1, skipna=True fixed random seeding bug in simulate_gbt that made multiprocessing batches have the same seed! batch handler: added rehasher to handle kiyo-type arrays, SHA224 and protocol=-1; still fiddling with file locking to avoid concurrency issues

correlate/pwrspec_estimation.py
@@ -238,8 +238,8 @@ def agg_stat_1d_pwrspec(pwr_1d):
     for index in range(n_runs):
         pwrmat_1d[index, :] = pwr_1d[index]['binavg']
 
-    mean_1d = np.mean(pwrmat_1d, axis=0)
-    std_1d = np.std(pwrmat_1d, axis=0)
+    mean_1d = np.mean(pwrmat_1d, axis=0, skipna=True)
+    std_1d = np.std(pwrmat_1d, axis=0, ddof=1, skipna=True)
     corrmat_1d = np.corrcoef(np.transpose(pwrmat_1d))
 
     return (mean_1d, std_1d, corrmat_1d)
@@ -296,8 +296,8 @@ def agg_stat_2d_pwrspec(pwr_2d, dataname='binavg'):
     for index in range(n_runs):
         pwrmat_2d[index, :, :] = pwr_2d[index][dataname]
 
-    mean_2d = np.mean(pwrmat_2d, axis=0)
-    std_2d = np.std(pwrmat_2d, axis=0)
+    mean_2d = np.mean(pwrmat_2d, axis=0, skipna=True)
+    std_2d = np.std(pwrmat_2d, axis=0, ddof=1, skipna=True)
 
     return (mean_2d, std_2d)
 

map/physical_gridding.py
@@ -70,8 +70,11 @@ def physical_grid(input_array, refinement=2, pad=5, order=2):
                        phys_dim[1], phys_dim[2], \
                        n[0], n[1], n[2])
 
-    phys_map = algebra.make_vect(np.zeros(n), axis_names=('freq', 'ra', 'dec'))
-    mask = np.ones_like(phys_map)
+    # this is wasteful in memory, but numpy can be pickled
+    phys_map_npy = np.zeros(n)
+    phys_map = algebra.make_vect(phys_map_npy, axis_names=('freq', 'ra', 'dec'))
+    #mask = np.ones_like(phys_map)
+    mask = np.ones_like(phys_map_npy)
 
     # TODO: should this be more sophisticated? N-1 or N?
     info = {}
@@ -120,9 +123,9 @@ def physical_grid(input_array, refinement=2, pad=5, order=2):
         interpol_grid[1, :, :] = gridx / angscale / thetax * numx + numx / 2
         interpol_grid[2, :, :] = gridy / angscale / thetay * numy + numy / 2
 
-        phys_map[i, :, :] = sp.ndimage.map_coordinates(input_array,
-                                                       interpol_grid,
-                                                       order=order)
+        phys_map_npy[i, :, :] = sp.ndimage.map_coordinates(input_array,
+                                                           interpol_grid,
+                                                           order=order)
 
         interpol_grid[1, :, :] = np.logical_or(interpol_grid[1, :, :] > numx,
                                              interpol_grid[1, :, :] < 0)
@@ -130,7 +133,14 @@ def physical_grid(input_array, refinement=2, pad=5, order=2):
                                              interpol_grid[1, :, :] < 0)
         mask = np.logical_not(np.logical_or(interpol_grid[1, :, :],
                                             interpol_grid[2, :, :]))
-        phys_map *= mask
+        phys_map_npy *= mask
+
+    return phys_map_npy, info
+
+
+def physkiyo(phys_grid_out):
+    phys_map = algebra.make_vect(phys_grid_out[0], axis_names=('freq', 'ra', 'dec'))
+    phys_map.info = phys_grid_out[1]
 
     return phys_map
 