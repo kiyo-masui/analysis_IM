0d927b354557471bc4fa1ad1ba4a866f3a104fb5
Tabitha Voytek <tcv@chime.cita.utoronto.ca>
2012-02-08 10:22:01 -0500

    New files for the differential gain calibration. Also additional plotting files.

---
+++ cal/flux_diff_gain_cal_gen.py
@@ -0,0 +1,242 @@
+"""Procedure to calculate only the flux and differential gain between XX and YY for each frequency from on-off scans of a calibrator such as 3C286.
+
+
+Need to update once complete.
+Run in analysis_IM: python cal/total_cal_gen_params.py input/tcv/mueller_gen_guppi.ini
+Note that the .ini file should indicate which session(s) and sourse you want to use. Script is run using data from a single source. The output is saved in a file called mueller_params_calc.txt
+ """
+import os
+
+from scipy.optimize import *
+import scipy as sp
+import numpy.ma as ma
+import numpy as np
+
+from kiyopy import parse_ini
+import kiyopy.utils
+import core.fitsGBT
+
+# Define a dictionary with keys the names of parameters to be read from
+# file and values the defaults.
+params_init = {
+               # Input and output.
+               "input_root" : "./",
+               "file_middles" : ("GBTdata",),
+               "input_end" : ".raw.acs.fits",
+               "output_root" : "calibrate",
+               "output_end" : '',
+               # Select data to process.
+               "scans" : (),
+               "IFs" : (),
+               "Guppi_test" : False,
+               }
+prefix = 'fgc_'
+
+
+class MuellerGen(object) :
+    """Calculates uncalibrated mueller matrix elements for each frequency using on-off scans at many separate parallactic angles. 
+    """
+   
+    def __init__(self, parameter_file_or_dict=None, feedback=2) :
+# Read the parameter file, store in dictionary named parameters.
+        self.params = parse_ini.parse(parameter_file_or_dict, params_init, 
+                                          prefix=prefix)
+        self.feedback = feedback
+
+    def peval(self,p,f):
+        d = self.d
+        XG = p[0]
+        YG = p[1]
+        theta = self.theta
+        t = self.function
+        
+        for i in range(0,len(t),4):
+            t[i] = XG*(0.5*(1+sp.cos(2*theta[i]))*d[i,f]+sp.sin(2*theta[i])*d[i+1,f]+0.5*(1-sp.cos(2*theta[i]))*d[i+3,f])
+            t[i+1] = 0
+            t[i+2] = 0
+            t[i+3] =YG*(0.5*(1-sp.cos(2*theta[i]))*d[i,f]-sp.sin(2*theta[i])*d[i+1,f]+0.5*(1+sp.cos(2*theta[i]))*d[i+3,f])
+        return t 
+
+    def residuals(self, p,errors, f,freq_val):
+#        Isrc = 19.6*pow((750.0/freq_val[f]),0.495)*2 
+#        Isrc = 19.6*pow((750.0/freq_val[f]),0.495)*(2.28315426-0.000484307905*freq_val[f]) # Added linear fit for Jansky to Kelvin conversion.
+        Isrc = 19.74748409*pow((750.0/freq_val[f]),0.49899785)*(2.28315426-0.000484307905*freq_val[f]) # My fit solution for 3C286
+#        Isrc = 25.15445092*pow((750.0/freq_val[f]),0.75578842)*(2.28315426-0.000484307905*freq_val[f]) # My fit solution for  3C48
+#        Isrc = 4.56303633*pow((750.0/freq_val[f]),0.59237327)*(2.28315426-0.000484307905*freq_val[f]) # My fit solution for 3C67
+        PAsrc = 33.0*sp.pi/180.0 # for 3C286, doesn't matter for unpolarized. 
+        Psrc = 0.07 #for 3C286 
+#        Psrc = 0 #for #3C48,3C67 
+        Qsrc = Isrc*Psrc*sp.cos(2*PAsrc) 
+        Usrc = Isrc*Psrc*sp.sin(2*PAsrc) 
+        Vsrc = 0
+        XXsrc = Isrc+Qsrc
+        YYsrc = Isrc-Qsrc
+        source = sp.zeros(4*self.file_num)
+        for i in range(0,len(source),4):
+            source[i] = XXsrc
+            source[i+1] = 0
+            source[i+2] = 0
+            source[i+3] = YYsrc
+        err = (source-self.peval(p,f))/errors
+        return err
+    
+    def execute(self, nprocesses=1) :
+        
+        params = self.params
+        kiyopy.utils.mkparents(params['output_root'])
+        parse_ini.write_params(params, params['output_root'] + 'params.ini',
+                               prefix=prefix)
+        guppi_result = params['Guppi_test']
+        output_root = params['output_root']
+        output_end = params['output_end']
+        file_name = params['file_middles'][0].split('/')[1]
+        print file_name
+        sess = file_name.split('_')[0]
+        print sess
+
+        self.file_num = len(params['file_middles']) # getting a variable for number of calibrator files being used
+
+# Need to remove count for calibrator files that are not the right size.
+        session_nums = sp.zeros(self.file_num)
+        c = 0
+        for file_middle in params['file_middles'] :
+            input_fname = (params['input_root'] + file_middle + 
+                           params['input_end'])
+            Reader = core.fitsGBT.Reader(input_fname)
+            n_scans = len(Reader.scan_set)
+            Len_set = Reader.read(0,0,force_tuple=True)
+#            session_nums[c] = file_middle.split('_')[0]
+#            print session_nums[c]
+            for Data in Len_set :
+                freq_num = Data.dims[3] # Setting the frequency binning to match whatever it's been set to. 
+            if guppi_result == True : 
+                if n_scans != 2 :
+                    self.file_num -=1
+            elif guppi_result == False :
+                if n_scans != 4 :
+                    self.file_num -=1
+            c+=1
+
+# Need to know the general frequency binning (going to assume that it's 200 for guppi, 260 for spectrometer, aka 1 MHz binning)
+#        if guppi_result == True :
+#            freq_num = 200 
+        if guppi_result == False :
+#            freq_num = 260    
+            self.file_num *= 2 #because there are two sets of scans per file for spectrometer, need to double the number of values
+
+
+        self.function = sp.zeros(4*self.file_num) #setting a variable of the right size for peval to use
+        self.theta = sp.zeros(4*self.file_num) #setting a variable for parallactic angle in radians
+        self.d = sp.zeros((4*self.file_num,freq_num)) #setting a variable for measured values
+        
+# Loop over files to process.      
+        k=0
+        for file_middle in params['file_middles'] :
+            input_fname = (params['input_root'] + file_middle +
+                           params['input_end'])
+# Read in the data, and loop over data blocks.
+            Reader = core.fitsGBT.Reader(input_fname)
+            n_IFs = len(Reader.IF_set) # Should be 1 given that we've stitched windows for the spectrometer or by def in guppi
+            n_scans = len(Reader.scan_set) #Should be 4 for the spectrometer, 2 for guppi
+            OnBlocks = Reader.read(range(0,n_scans,2),0,force_tuple=True)
+            OffBlocks = Reader.read(range(1,n_scans,2),0,force_tuple=True)
+#force_tuple=True makes the ouput of Reader.read a tuple even if thre is only one Block to return.
+            Blocks = Reader.read(params['scans'], params['IFs'],
+                                 force_tuple=True)
+
+# Setting labels for indices for later
+            on_ind = 0
+            off_ind = 1
+            XX_ind = 0
+            YY_ind = 3
+            XY_ind = 1
+            YX_ind = 2
+            
+#Calculating Parallactic angles for the cal file
+            PA = sp.zeros(n_scans)
+            m = 0           
+            for Data in Blocks:
+                freq_len = Data.dims[3]
+                Data.calc_freq()
+                freq_val = Data.freq
+                freq_val = freq_val/1000000       
+                Data.calc_PA()
+                PA[m] = ma.mean(Data.PA)  
+                m+=1
+            
+#Building the measured data into arrays (guppi version)         
+            if guppi_result == True : 
+                if n_scans == 2 : 
+                    self.theta[k] = ma.mean(PA)
+                    self.theta[k+1] = ma.mean(PA)
+                    self.theta[k+2] = ma.mean(PA)
+                    self.theta[k+3] = ma.mean(PA)
+
+                    S_med_calon_src = sp.zeros((freq_len,4))
+                    S_med_caloff_src = sp.zeros((freq_len,4))
+                    S_med_calon = sp.zeros((freq_len,4))
+                    S_med_caloff = sp.zeros((freq_len,4))
+
+                    for Data in OnBlocks:
+                        S_med_caloff_src[:,0] = ma.median(Data.data[:,XX_ind,off_ind,:],axis=0)
+                        S_med_caloff_src[:,1] = ma.median(Data.data[:,XY_ind,off_ind,:],axis=0)
+                        S_med_caloff_src[:,2] = ma.median(Data.data[:,YX_ind,off_ind,:],axis=0)
+                        S_med_caloff_src[:,3] = ma.median(Data.data[:,YY_ind,off_ind,:],axis=0)
+
+                        S_med_calon_src[:,0] = ma.median(Data.data[:,XX_ind,on_ind,:],axis=0)
+                        S_med_calon_src[:,1] = ma.median(Data.data[:,XY_ind,on_ind,:],axis=0)
+                        S_med_calon_src[:,2] = ma.median(Data.data[:,YX_ind,on_ind,:],axis=0)
+                        S_med_calon_src[:,3] = ma.median(Data.data[:,YY_ind,on_ind,:],axis=0)
+                    
+                    for Data in OffBlocks:
+                        S_med_caloff[:,0] = ma.median(Data.data[:,XX_ind,off_ind,:],axis=0)
+                        S_med_caloff[:,1] = ma.median(Data.data[:,XY_ind,off_ind,:],axis=0)
+                        S_med_caloff[:,2] = ma.median(Data.data[:,YX_ind,off_ind,:],axis=0)
+                        S_med_caloff[:,3] = ma.median(Data.data[:,YY_ind,off_ind,:],axis=0)
+ 
+                        S_med_calon[:,0] = ma.median(Data.data[:,XX_ind,on_ind,:],axis=0)
+                        S_med_calon[:,1] = ma.median(Data.data[:,XY_ind,on_ind,:],axis=0)
+                        S_med_calon[:,2] = ma.median(Data.data[:,YX_ind,on_ind,:],axis=0)
+                        S_med_calon[:,3] = ma.median(Data.data[:,YY_ind,on_ind,:],axis=0)
+ 
+                     
+                    self.d[k,:] = 0.5*(S_med_calon_src[:,0]+S_med_caloff_src[:,0]-S_med_calon[:,0]-S_med_caloff[:,0])
+                    self.d[k+1,:] = 0.5*(S_med_calon_src[:,1]+S_med_caloff_src[:,1]-S_med_calon[:,1]-S_med_caloff[:,1])
+                    self.d[k+2,:] = 0.5*(S_med_calon_src[:,2]+S_med_caloff_src[:,2]-S_med_calon[:,2]-S_med_caloff[:,2])
+                    self.d[k+3,:] = 0.5*(S_med_calon_src[:,3]+S_med_caloff_src[:,3]-S_med_calon[:,3]-S_med_caloff[:,3])
+                    k+=4
+
+        for a in range(0,4*self.file_num):
+            for b in range(0,freq_num):
+#                print self.d[a,b]
+                if self.d[a,b] > 1000 :
+                   self.d[a,b] = 1000
+
+        #There are 2 parameters for this version p[0] is XX gain and p[1] is YY gain. 
+        p0 = [1,1] # guessed preliminary values
+        error = sp.ones(4*self.file_num)
+        #Note that error can be used to weight the equations if not all set to one.
+
+        p_val_out = sp.zeros((freq_len, 3))
+ #       p_err_out = sp.zeros((freq_len, 17))
+     
+        for f in range(0,freq_len):   
+            plsq = leastsq(self.residuals,p0,args=(error,f,freq_val),full_output=0, maxfev=5000)
+            pval = plsq[0] # this is the 1-d array of results0
+
+            p_val_out[f,0] = freq_val[f]
+            p_val_out[f,1] = pval[0]
+            p_val_out[f,2] = pval[1]
+
+#        sess_num = int(session_nums[0])
+#        print sess_num
+#        np.savetxt(output_root+str(sess_num)+'_flux_mueller_matrix_calc'+output_end, p_val_out, delimiter = ' ')
+        out_path = output_root+sess+'_diff_gain_calc'+output_end
+        np.savetxt(out_path,p_val_out,delimiter = ' ')
+#        np.savetxt('mueller_params_error.txt', p_err_out, delimiter = ' ')
+
+#If this file is run from the command line, execute the main function.
+if __name__ == "__main__":
+    import sys
+    MuellerGen(str(sys.argv[1])).execute()
+
---
+++ scripts/plot_3D_mueller_data.py
@@ -0,0 +1,218 @@
+import pylab
+import sys
+from numpy import *
+import scipy as sp
+
+prefix = ''
+
+#This is $GBT_OUT/mueller_params/
+filedir = sys.argv[1]
+suffix = '_mueller_matrix_from_inverted_params.txt'
+
+#Currently set for the range of all 10B_036 with guppi
+#May want to rebuild depending what sessions you want to grab.
+directory = 'GBT10B_036'
+directory2 = 'GBT11B_055'
+min_sess = 41
+min_sess2 = 01
+max_sess = 89
+max_sess2 = 18
+#Number of sessions used
+len = max_sess - min_sess
+len2 = max_sess2-min_sess2
+sessions = []
+filenames = []
+for i in range(0,len):
+   label = min_sess+i
+   if label<10:
+      label = '0'+str(label)
+   else:
+      label = str(label)
+   filenames.append(filedir+directory+'/'+label+suffix)
+   sessions.append(label)
+for i in range(0,len2):
+   label = min_sess2+i
+   if label<10:
+      label = '0'+str(label)
+   else:
+      label = str(label)
+   filenames.append(filedir+directory2+'/'+label+suffix)
+   sessions.append(label)
+
+#print filenames
+
+#Need a test_dir to determine the number of frequency slices
+array =[] 
+test_dir = loadtxt(filenames[0])
+scale = shape(test_dir)
+#print array
+
+#Database of mueller files
+tot_len = len+len2
+for j in range(0,tot_len):
+   source = filenames[j]
+   data = loadtxt(source)
+   array.append(data)
+#print array[0][0][1]
+#print array[1][0][1]
+
+#List of frequencies for axis.
+freqs = test_dir[:,0]
+
+#print freqs
+
+m_II = sp.zeros((tot_len,scale[0]))
+m_IQ = sp.zeros((tot_len,scale[0]))
+m_IU = sp.zeros((tot_len,scale[0]))
+m_IV = sp.zeros((tot_len,scale[0]))
+m_QI = sp.zeros((tot_len,scale[0]))
+m_QQ = sp.zeros((tot_len,scale[0]))
+m_QU = sp.zeros((tot_len,scale[0]))
+m_QV = sp.zeros((tot_len,scale[0]))
+m_UI = sp.zeros((tot_len,scale[0]))
+m_UQ = sp.zeros((tot_len,scale[0]))
+m_UU = sp.zeros((tot_len,scale[0]))
+m_UV = sp.zeros((tot_len,scale[0]))
+m_VI = sp.zeros((tot_len,scale[0]))
+m_VQ = sp.zeros((tot_len,scale[0]))
+m_VU = sp.zeros((tot_len,scale[0]))
+m_VV = sp.zeros((tot_len,scale[0]))
+
+#Building separate tables for each matrix element.
+for i in range(0,tot_len):
+   for j in range(0,scale[0]):
+      m_II[i,j] = array[i][j][1]
+      #All terms other than m_II normalized w/ respect to m_II
+      m_IQ[i,j] = array[i][j][2]/m_II[i,j]
+      m_IU[i,j] = array[i][j][3]/m_II[i,j]
+      m_IV[i,j] = array[i][j][4]/m_II[i,j]
+      m_QI[i,j] = array[i][j][5]/m_II[i,j]
+      m_QQ[i,j] = array[i][j][6]/m_II[i,j]
+      m_QU[i,j] = array[i][j][7]/m_II[i,j]
+      m_QV[i,j] = array[i][j][8]/m_II[i,j]
+      m_UI[i,j] = array[i][j][9]/m_II[i,j]
+      m_UQ[i,j] = array[i][j][10]/m_II[i,j]
+      m_UU[i,j] = array[i][j][11]/m_II[i,j]
+      m_UV[i,j] = array[i][j][12]/m_II[i,j]
+      m_VI[i,j] = array[i][j][13]/m_II[i,j]
+      m_VQ[i,j] = array[i][j][14]/m_II[i,j]
+      m_VU[i,j] = array[i][j][15]/m_II[i,j]
+      m_VV[i,j] = array[i][j][16]/m_II[i,j]
+
+#print m_II
+
+#M_II
+#pylab.subplot(441)
+#pylab.imshow(m_II/m_II,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.,vmax=1.,extent=(freqs.min(),freqs.max(),min_sess,max_sess))
+pylab.imshow(m_II,interpolation='gaussian', cmap='hot', origin='lower',vmin=0.5,vmax=2.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal')
+pylab.savefig('m_II_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf()
+
+#M_IQ
+#pylab.subplot(442)
+pylab.imshow(m_IQ,interpolation='gaussian', cmap='hot', origin='lower',vmin=-0.5,vmax=0.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal')
+pylab.savefig('m_IQ_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf()
+
+#M_IU 
+#pylab.subplot(443)
+pylab.imshow(m_IU,interpolation='gaussian', cmap='hot', origin='lower',vmin=-0.06,vmax=0.06,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_IU_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_IV 
+#pylab.subplot(444)
+pylab.imshow(m_IV,interpolation='gaussian', cmap='hot', origin='lower',vmin=-0.1,vmax=0.1,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_IV_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_QI 
+#pylab.subplot(445)
+pylab.imshow(m_QI,interpolation='gaussian', cmap='hot', origin='lower',vmin=-0.5,vmax=0.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_QI_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_QQ
+#pylab.subplot(446)
+pylab.imshow(m_QQ,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.5,vmax=1.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_QQ_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_QU
+#pylab.subplot(447)
+pylab.imshow(m_QU,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.5,vmax=1.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_QU_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_QV
+#pylab.subplot(448)
+pylab.imshow(m_QV,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.5,vmax=1.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_QV_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_UI 
+#pylab.subplot(449)
+pylab.imshow(m_UI,interpolation='gaussian', cmap='hot', origin='lower',vmin=-0.5,vmax=0.5,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_UI_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_UQ
+#pylab.subplot(4,4,10)
+pylab.imshow(m_UQ,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.25,vmax=1.25,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_UQ_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_UU 
+#pylab.subplot(4,4,11)
+pylab.imshow(m_UU,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.25,vmax=1.25,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_UU_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_UV
+#pylab.subplot(4,4,12)
+pylab.imshow(m_UV,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.25,vmax=1.25,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_UV_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_VI
+#pylab.subplot(4,4,13)
+pylab.imshow(m_VI,interpolation='gaussian', cmap='hot', origin='lower',vmin=-0.25,vmax=0.25,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_VI_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_VQ 
+#pylab.subplot(4,4,14)
+pylab.imshow(m_VQ,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1,vmax=1,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_VQ_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_VU 
+#pylab.subplot(4,4,15)
+pylab.imshow(m_VU,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.25,vmax=1.25,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_VU_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#M_VV
+#pylab.subplot(4,4,16)
+pylab.imshow(m_VV,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.25,vmax=1.25,extent=(freqs.max(),freqs.min(),0,tot_len))
+pylab.colorbar(orientation='horizontal') 
+pylab.savefig('m_VV_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+pylab.clf() 
+
+#pylab.savefig('mueller_'+directory+'_'+str(min_sess)+'-'+str(max_sess)+'_compare.png')
+#pylab.clf()
