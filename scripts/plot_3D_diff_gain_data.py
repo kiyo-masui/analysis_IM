fbac53a98dad74130e8f2fa920e3fec8453c75ad
Tabitha Voytek <tcv@prawn.cita.utoronto.ca>
2012-05-10 10:37:43 -0400

    edits for usage

--- scripts/plot_3D_diff_gain_data.py
+++ scripts/plot_3D_diff_gain_data.py
@@ -16,13 +16,17 @@ suffix = '_diff_gain_calc.txt'
 #May want to rebuild depending what sessions you want to grab.
 directory = 'GBT10B_036'
 directory2 = 'GBT11B_055'
+directory3 = 'GBT12A_418'
 min_sess = 41
 min_sess2 = 01
+min_sess3 = 01
 max_sess = 90
 max_sess2 = 19
+max_sess3 = 03
 #Number of sessions used
 len = max_sess - min_sess
 len2 = max_sess2-min_sess2
+len3 = max_sess3-min_sess3
 sessions = []
 filenames = []
 for i in range(0,len):
@@ -41,9 +45,17 @@ for i in range(0,len2):
       label = str(label)
    filenames.append(filedir+directory2+'/'+label+suffix)
    sessions.append(label)
+for i in range(0,len3):
+   label = min_sess3+i
+   if label<10:
+      label ='0'+str(label)
+   else:
+      label = str(label)
+   filenames.append(filedir+directory3+'/'+label+suffix)
+   sessions.append(label)
 
-print sessions
-print filenames
+#print sessions
+#print filenames
 
 #Need a test_dir to determine the number of frequency slices
 array =[] 
@@ -52,7 +64,7 @@ scale = shape(test_dir)
 #print array
 
 #Database of mueller files
-tot_len = len+len2
+tot_len = len+len2+len3
 for j in range(0,tot_len):
    source = filenames[j]
    data = loadtxt(source)
@@ -70,6 +82,8 @@ XG = sp.zeros((tot_len,scale[0]))
 YG = sp.zeros((tot_len,scale[0]))
 XG_mod = sp.zeros((tot_len,scale[0]))
 YG_mod = sp.zeros((tot_len,scale[0]))
+XG_comp = sp.zeros((tot_len,scale[0]))
+YG_comp = sp.zeros((tot_len,scale[0]))
 
 #Building separate tables for each param.
 for i in range(0,tot_len):
@@ -94,20 +108,28 @@ for i in range(0,tot_len):
    XG_mod[i] = Xmod(freqs_new)
    YG_mod[i] = Ymod(freqs_new)
    
-
+for i in range(0,tot_len):
+   for j in range(0,scale[0]):
+      XG_comp[i,j] = abs(XG[i,j]-XG[67,j])/XG[67,j]
+      YG_comp[i,j] = abs(YG[i,j]-YG[67,j])/YG[67,j]
 
 #Going to make the two gains as subplots
+pylab.suptitle('XX and YY Gain Correction Factors (Scale is 3.25-0.75)')
 pylab.subplot(211)
 #pylab.imshow(m_II/m_II,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.,vmax=1.,extent=(freqs.min(),freqs.max(),min_sess,max_sess))
 pylab.imshow(XG, cmap='hot', origin='lower',vmin=0.75,vmax=3.25,extent=(freqs.max(),freqs.min(),0,tot_len))
-pylab.colorbar(orientation='horizontal')
+#pylab.colorbar(orientation='horizontal')
+#pylab.xlabel('Frequency (MHz)')
+pylab.ylabel('Session')
 #pylab.savefig('XX_Gain_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
 #pylab.clf()
 
 pylab.subplot(212)
 pylab.imshow(YG, cmap='hot', origin='lower',vmin=0.75,vmax=3.25,extent=(freqs.max(),freqs.min(),0,tot_len))
-pylab.colorbar(orientation='horizontal')
-pylab.savefig('XX_and_YY_gain_'+str(min_sess)+'-'+str(max_sess2-1)+'_compare.png')
+#pylab.colorbar(orientation='horizontal')
+pylab.xlabel('Frequency (MHz)')
+pylab.ylabel('Session')
+pylab.savefig('XX_and_YY_gain_'+str(min_sess)+'-'+str(max_sess-1)+'_and_'+str(min_sess2)+'-'+str(max_sess2-1)+'_and_'+str(min_sess3)+ '-'+str(max_sess3-1)+'_compare.png')
 pylab.clf()
 
 pylab.subplot(211)
@@ -119,3 +141,23 @@ pylab.imshow(YG_mod,cmap='hot',origin='lower',vmin=0.75,vmax=3.25,extent=(freqs_
 pylab.colorbar(orientation='horizontal')
 pylab.savefig('XX_and_YY_gain_smoothed_'+str(min_sess)+'-'+str(max_sess2-1)+'_compare.png')
 pylab.clf()
+
+# Plotting deviation from a reference correction factor 
+pylab.suptitle('XX and YY Gain Correction Factor Deviation (deltaG/G, max = 15%) ')
+pylab.subplot(211)
+#pylab.imshow(m_II/m_II,interpolation='gaussian', cmap='hot', origin='lower',vmin=-1.,vmax=1.,extent=(freqs.min(),freqs.max(),min_sess,max_sess))
+pylab.imshow(XG_comp, cmap='hot', origin='lower',vmin=0,vmax=0.15,extent=(freqs.max(),freqs.min(),0,tot_len))
+#pylab.colorbar(orientation='horizontal')
+#pylab.xlabel('Frequency (MHz)')
+pylab.ylabel('Session')
+#pylab.savefig('XX_Gain_'+str(min_sess)+'-'+str(max_sess2)+'_compare.png')
+#pylab.clf()
+
+pylab.subplot(212)
+pylab.imshow(YG_comp, cmap='hot', origin='lower',vmin=0,vmax=0.15,extent=(freqs.max(),freqs.min(),0,tot_len))
+#pylab.colorbar(orientation='horizontal')
+pylab.xlabel('Frequency (MHz)')
+pylab.ylabel('Session')
+pylab.savefig('XX_and_YY_gain_'+str(min_sess)+'-'+str(max_sess-1)+'_and_'+str(min_sess2)+'-'+str(max_sess2-1)+'_and_'+str(min_sess3)+ '-'+str(max_sess3-1)+'_correction_variation_from_2(418).png')
+pylab.clf()
+
