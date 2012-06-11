fa2a6fbdcfd96efa163cb27ab6123b2689d9a75d
Tabitha Voytek <tcv@prawn.cita.utoronto.ca>
2012-06-11 17:29:51 -0400

    additional work on calibration, including noise cal phase stuff.

--- cal/calibration_comparison.py
+++ cal/calibration_comparison.py
@@ -1,9 +1,8 @@
-"""Procedure to calculate the noise cal phase that causes some of the U signal to bleed into the V signal for the data. This expected to be at least session stable (if not more) as this phase is reset whenever guppi is started. Setting up the calcualtion of the phase using the point source data. 
+"""IN PROGRESS: Trying to calculate the variation in calibrated data.
 
-Using the point source data, want to take the ratio of U/sqrt(U**2+V**2) as a function of frequency and cos(2*phi), where phi is the noise cal phase. 
-
-Run in analysis_IM: python cal/noise_cal_phase.py input/tcv/noise_cal_gen.ini
-Note that the .ini file should indicate which session(s) and sourse you want to use. Script is run using data from a single source. The output is saved in a file called point_source_differential_phase_compare.txt
+Run information still needs to be updated:
+Run in analysis_IM: python cal/total_cal_gen_params.py input/tcv/mueller_gen_guppi.ini
+Note that the .ini file should indicate which session(s) and sourse you want to use. Script is run using data from a single source. The output is saved in a file called mueller_params_calc.txt
  """
 import os
 
@@ -150,124 +149,172 @@ class MuellerGen(object) :
                         S_med_calon[:,2] = ma.median(Data.data[:,V_ind,on_ind,:],axis=0)
                         S_med_calon[:,3] = ma.median(Data.data[:,Y_ind,on_ind,:],axis=0)
  
-# We want Tcal as the source:
-                    self.d[k,:] = S_med_calon_src[:,0]-S_med_caloff_src[:,0]
-                    self.d[k+1,:] = S_med_calon_src[:,1]-S_med_caloff_src[:,1]
-                    self.d[k+2,:] = S_med_calon_src[:,2]-S_med_caloff_src[:,2]
-                    self.d[k+3,:] = S_med_calon_src[:,3]-S_med_caloff_src[:,3]
-                    
-#                    self.d[k,:] = 0.5*(S_med_calon_src[:,0]+S_med_caloff_src[:,0]-S_med_calon[:,0]-S_med_caloff[:,0])
-#                    self.d[k+1,:] = 0.5*(S_med_calon_src[:,1]+S_med_caloff_src[:,1]-S_med_calon[:,1]-S_med_caloff[:,1])
-#                    self.d[k+2,:] = 0.5*(S_med_calon_src[:,2]+S_med_caloff_src[:,2]-S_med_calon[:,2]-S_med_caloff[:,2])
-#                    self.d[k+3,:] = 0.5*(S_med_calon_src[:,3]+S_med_caloff_src[:,3]-S_med_calon[:,3]-S_med_caloff[:,3])
+# If We want Tcal as the source:
+#                    self.d[k,:] = S_med_calon_src[:,0]-S_med_caloff_src[:,0]
+#                    self.d[k+1,:] = S_med_calon_src[:,1]-S_med_caloff_src[:,1]
+#                    self.d[k+2,:] = S_med_calon_src[:,2]-S_med_caloff_src[:,2]
+#                    self.d[k+3,:] = S_med_calon_src[:,3]-S_med_caloff_src[:,3]
+# If we want Tsrc as the source:                    
+                    self.d[k,:] = 0.5*(S_med_calon_src[:,0]+S_med_caloff_src[:,0]-S_med_calon[:,0]-S_med_caloff[:,0])
+                    self.d[k+1,:] = 0.5*(S_med_calon_src[:,1]+S_med_caloff_src[:,1]-S_med_calon[:,1]-S_med_caloff[:,1])
+                    self.d[k+2,:] = 0.5*(S_med_calon_src[:,2]+S_med_caloff_src[:,2]-S_med_calon[:,2]-S_med_caloff[:,2])
+                    self.d[k+3,:] = 0.5*(S_med_calon_src[:,3]+S_med_caloff_src[:,3]-S_med_calon[:,3]-S_med_caloff[:,3])
                     k+=4
 
         for a in range(0,4*self.file_num):
             for b in range(0,freq_num):
 #                print self.d[a,b]
-                if self.d[a,b] > 3 :
-                   self.d[a,b] = 3
-                elif self.d[a,b]<-3:
-                   self.d[a,b] = -3
+                if self.d[a,b] > 100:
+                   self.d[a,b] = 100
+                elif self.d[a,b]<-100:
+                   self.d[a,b] = -100
+        
+#        print self.theta
 
-#        fitfunc = lambda p,x: sp.arccos(sp.cos(p*x))
-#        fitfunc = lambda p,x: sp.arccos(sp.cos(p[0]*x+p[1]))
-        fitfunc = lambda p,x: sp.cos(p[0]*x +p[1])
-#        fitfunc = lambda p,x:0.9*sp.cos(2*sp.pi/84*x+p[2]%(2*sp.pi))
+        fitfunc = lambda p,x: p[0]*x+p[1]
+#        fitfunc = lambda p,x: p[0]+p[1]*pow((750.0/x),p[2])
+#        fitfunc = lambda p,x: p[0]*pow((750.0/x),p[1]) 
         errfunc = lambda p, x, y: fitfunc(p,x)-y
-        fitfunc2 = lambda q,x: q[0]*sp.sin(q[1]*x+q[2])
-        errfunc2 = lambda q,x,y: fitfunc2(q,x)-y
-        fitfunc3 = lambda s, x: sp.sin(2*(s[0]*x+s[1]%(2*sp.pi)))
-        errfunc3 = lambda s,x,y: fitfunc3(s,x)-y
-#        fitfunc3 = lambda s,x: sp.tan(2*sp.pi*s[0]+s[1])
-#        errfunc3 = lambda s,x,y: sp.arctan(fitfunc3(s,x))-sp.arctan(y)
-
-#        freqs = []
-#        U_trunc = []
-#        V_trunc = []
-#        Mag_trunc = []
-#        for i in range(245,150,-1):
-#             freqs.append(freq_val[i])
-#             U_trunc.append(self.d[2,i])
-#             V_trunc.append(self.d[3,i])
-#             Mag_trunc.append(sp.sqrt(self.d[2,i]**2+self.d[3,i]**2))
-#        print freqs
-#        print len(freqs)
-#        print len(Mag_trunc)
-#        print Mag_trunc
-#        pylab.scatter(freqs,U_trunc,s=1,c='c',edgecolors='none')
-#        pylab.scatter(freqs,V_trunc,s=1,c='b',edgecolors='none')
-#        pylab.scatter(freqs,Mag_trunc,s=1,c='g',edgecolors='none')
-#        m0 = [0.1,20.0,1.0]
-#        M,success = optimize.leastsq(errfunc,m0[:],args=(freqs,Mag_trunc))
-#       pylab.plot(freqs,fitfunc(M,freqs),c='r') 
-#       print freq_val
-        freqs = sp.zeros(len(freq_val))
-        U_data = sp.zeros((self.file_num,len(freqs)))
-        V_data = sp.zeros((self.file_num,len(freqs)))
-        R_data = sp.zeros((self.file_num,len(freqs)))
+        freqs = sp.zeros((self.file_num,len(freq_val)))
+        U_data = sp.zeros((self.file_num,len(freq_val)))
+        V_data = sp.zeros((self.file_num,len(freq_val)))
+        X_data = sp.zeros((self.file_num,len(freq_val)))
+        Y_data = sp.zeros((self.file_num,len(freq_val)))
+        I_data = sp.zeros((self.file_num,len(freq_val)))
+        Q_data = sp.zeros((self.file_num,len(freq_val)))
+        R_data = sp.zeros((self.file_num,len(freq_val)))
+        I_src = sp.zeros((self.file_num,len(freq_val)))
+        Q_src = sp.zeros((self.file_num,len(freq_val)))
+        U_src = sp.zeros((self.file_num,len(freq_val)))
+        V_src = sp.zeros((self.file_num,len(freq_val)))
         for i in range(0,len(freq_val)):
-#            freqs[i] = freq_val[i]
-            freqs[i] = freq_val[len(freq_val)-i-1]
             for j in range(0,self.file_num):
+                freqs[j,i] = freq_val[len(freq_val)-i-1]
+#           for j in range(0,self.file_num):
+# If input is in XYformat
                 U_data[j,i] = self.d[4*j+1,len(freq_val)-i-1]
                 V_data[j,i] = self.d[4*j+2,len(freq_val)-i-1]
+                X_data[j,i] = self.d[4*j,len(freq_val)-i-1]
+                Y_data[j,i] = self.d[4*j+3,len(freq_val)-i-1]
+# If input is in IQ format
+#                I_data[j,i] = self.d[4*j,len(freq_val)-i-1]
+#                Q_data[j,i] = self.d[4*j+1,len(freq_val)-i-1]
+#                U_data[j,i] = self.d[4*j+2,len(freq_val)-i-1]
+#                V_data[j,i] = self.d[4*j+3,len(freq_val)-i-1]
+
 #               R_data[j,i] = sp.sin(2*sp.arctan(V_data[j,i]/U_data[j,i]))
-#                R_data[j,i] = U_data[j,i]
-#                R_data[j,i] = sp.arccos(U_data[j,i]/sp.sqrt(U_data[j,i]**2+ V_data[j,i]**2))
-                R_data[j,i] = U_data[j,i]/sp.sqrt(U_data[j,i]**2+V_data[j,i]**2)
+#                R_data[j,i] = U_data[j,i]/sp.sqrt(U_data[j,i]**2+ V_data[j,i]**2)
+                
+#        print np.any(np.isnan(R_data))
+#        print np.any(np.isinf(R_data))
+#        R_fin = sp.zeros((2,self.file_num))
 
-#        print R_data[:,0]                
-#        print R_data[:,-1]
-        print np.any(np.isnan(R_data))
-        print np.any(np.isinf(R_data))
-        R_fin = sp.zeros((2,self.file_num))
-        print freqs
+                I_src[j,i] = 19.74748409*pow((750.0/freqs[j,i]),0.49899785)*(2.28315426-0.000484307905*freqs[j,i]) # My fit solution for 3C286
+#        Isrc = 25.15445092*pow((750.0/freq_val[f]),0.75578842)*(2.28315426-0.000484307905*freq_val[f]) # My fit solution for  3C48
+                PAsrc = 33.0*sp.pi/180.0 # for 3C286, doesn't matter for unpolarized. 
+                Psrc = 0.07 #for 3C286 
+#        Psrc = 0 #for #3C48,3C67, 3C147, 3C295
+                Q_src[j,i] = I_src[j,i]*Psrc*sp.cos(2*PAsrc)
+                U_src[j,i] = I_src[j,i]*Psrc*sp.sin(2*PAsrc)
+#        Vsrc = 0
+#        XXsrc0 = Isrc-Qsrc
+#        YYsrc0 = Isrc+Qsrc
+        pI_out = sp.zeros((2,self.file_num))
+        pQ_out = sp.zeros((3,self.file_num))
+        pU_out = sp.zeros((3,self.file_num))
+        pV_out = sp.zeros((3,self.file_num))
+        for i in range(0,self.file_num):     
+#            pI0 = [1.0,1.0,0.5]
+            pI0 = [0.0,0.0]
+#            I_diff = I_src[i,:]-I_data[i,:]
+            I_diff = I_src[i,:] - 0.5*(X_data[i,:]+Y_data[i,:])
+            print np.mean(I_diff)
+            print np.std(I_diff)
+            for j in range(0,len(freqs[i,:])):
+                if I_diff[j]>2:
+                    I_diff[j] = 2
+                elif I_diff[j]<-2:
+                    I_diff[j] = -2
+
+            print np.mean(I_diff)
+            print np.std(I_diff)
+
+            pI = optimize.leastsq(errfunc,pI0[:],args=(freqs[i,:],I_diff),full_output=1)
+            pI_out[:,i] = pI[0]
+            print pI[1]
+            print pI[0]
+
+#            pQ0 = [1.0,1.0,0.5]
+#            Q_diff = Q_src[i,:]*sp.cos(2*self.theta[i])-U_src[i,:]*sp.sin(2*self.theta[i])-Q_data[i,:]
+#            Q_diff = Q_src[i,:]-0.5*(Y_data[i,:]-X_data[i,:])
+            Q_diff = Q_src[i,:]*sp.cos(2*self.theta[i])-U_src[i,:]*sp.sin(2*self.theta[i])-0.5*(Y_data[i,:]-X_data[i,:])
+#            pQ = optimize.leastsq(errfunc,pQ0[:],args=(freqs[i,:],Q_diff),full_output=1)
+#            pU0 = [1.0,1.0,0.5]
+#            U_diff = -Q_src[i,:]*sp.sin(2*self.theta[i])+U_src[i,:]*sp.cos(2*self.theta[i])-U_data[i,:] 
+            U_diff = -Q_src[i,:]*sp.sin(2*self.theta[i])+U_src[i,:]*sp.cos(2*self.theta[i])-U_data[i,:]
+#            U_diff = U_src[i,:]-U_data[i,:]
+#            pU = optimize.leastsq(errfunc,pU0[:],args=(freqs[i,:],U_diff),full_output=1)
+#            pV0 = [1.0,1.0,0.5]
+#            pV = optimize.leastsq(errfunc,pV0[:],args=(freqs[i,:],V_src[i,:]-V_data[i,:]),full_output=1)       
+        
+#            pylab.plot(freqs[i,:],X_data[i,:],label='X'+str(i))
+#            pylab.plot(freqs[i,:],Y_data[i,:],label='Y'+str(i))
+
+#Last session examined
+        pylab.plot(freqs[-1,:],fitfunc(pI_out[:,-1],freqs[-1,:]),c='r',label = 'I_line1')
+        pylab.scatter(freqs[-1,:],I_diff[:],s=4,c='r',edgecolors='none',label='I_diff1')
+#        pylab.scatter(freqs[-1,:],Q_diff[:],s=4,c='c',edgecolors='none',label='Q_diff')
+#        pylab.scatter(freqs[-1,:],U_diff[:],s=4,c='g',edgecolors='none',label='U_diff')
+#        pylab.scatter(freqs[-1,:],-V_data[-1,:],s=4,c='y',edgecolors='none',label='V_diff')
+#First session examined (fdg)
+        pylab.plot(freqs[0,:],fitfunc(pI_out[:,0],freqs[0,:]),c='m',label='I_line2')
+        pylab.scatter(freqs[0,:],I_src[0,:]-0.5*(X_data[0,:]+Y_data[0,:]),s=4,c='m',edgecolors='none',label='I_diff2')
+#        pylab.scatter(freqs[0,:],Q_src[0,:]*sp.cos(2*self.theta[0])+U_src[0,:]*sp.sin(2*self.theta[0])-0.5*(Y_data[0,:]-X_data[0,:]),s=4,c='c',edgecolors='none',label='Q_diff')
+#        pylab.scatter(freqs[0,:],-Q_src[0,:]*sp.sin(2*self.theta[0])+U_src[0,:]*sp.cos(2*self.theta[0])-U_data[0,:],s=4,c='g',edgecolors='none',label='U_diff')
+#        pylab.scatter(freqs[0,:],V_src[-0,:]-V_data[0,:],s=4,c='y',edgecolors='none',label='V_diff')
+# First session examined (pulsar)
+#        pylab.scatter(freqs[0,:],I_src[0,:]-I_data[0,:],s=4,c='m',edgecolors='none',label='I_diff')
+#        pylab.scatter(freqs[0,:],Q_src[0,:]*sp.cos(2*self.theta[0])-U_src[0,:]*sp.sin(2*self.theta[0])-Q_data[0,:],s=4,c='c',edgecolors='none',label='Q_diff')
+#        pylab.scatter(freqs[0,:],Q_src[0,:]*sp.sin(2*self.theta[0])+U_src[0,:]*sp.cos(2*self.theta[0])-U_data[0,:],s=4,c='g',edgecolors='none',label='U_diff')
+#        pylab.scatter(freqs[0,:],V_src[-0,:]-V_data[0,:],s=4,c='y',edgecolors='none',label='V_diff')
+
+#        pylab.plot(freqs[0,:],fitfunc(pI_out[:,0],freqs[0,:]),c='b')
+#        pylab.scatter(freqs[0,:],pI_out[1,:],c='g')
+        pylab.ylim(-2,2)
+
  
-        for i in range(0,self.file_num):
-            for j in range(0,len(freqs)):
-                if int(freqs[j])==710:
-                    mask_num = j
-                if int(freqs[j])==740:
-                    mask_num2 = j
-#            mask_num = 7 #705.859 in frequency space
-#            mask_num2 = 24 #719.140625 in frequency space
-#            mask_num = 101 #775.390625 in frequency space
-#            mask_num2 = 121 #790.234375 in frequency space
-#            mask_num = 0
-#            mask_num2 = -1
+#        for i in range(0,self.file_num):
+#            mask_num = 5 # if working with 256 values
+#            mask_num = 200 # if working with 4096 values
 
-            Datain = R_data[i,mask_num:mask_num2]
-            fin = freqs[mask_num:mask_num2]
+#            Datain = R_data[i,mask_num:-mask_num]
+#            fin = freqs[mask_num:-mask_num]
 
-            bad_pts = np.logical_or(np.isnan(Datain), np.isinf(Datain))
-	    good_ind = np.where(np.logical_not(bad_pts))
-            Datain = Datain[good_ind]
-            fin = fin[good_ind]
+#            bad_pts = np.logical_or(np.isnan(Datain), np.isinf(Datain))
+#	    good_ind = np.where(np.logical_not(bad_pts))
+#            Datain = Datain[good_ind]
+#            fin = fin[good_ind]
 
 # settings: 0.08, 0.10,0.12,0.16
-            R0 = [0.18,1.0]
-#            R0 = [0.08,]
-            R,success = optimize.leastsq(errfunc,R0[:],args=(fin,Datain),maxfev=10000)
-            R[1] = R[1]%(2*sp.pi)
-            print success
+#            R0 = [0.16,1.0]
+#            R,success = optimize.leastsq(errfunc,R0[:],args=(fin,Datain),maxfev=10000)
+#            R[1] = R[1]%(2*sp.pi)
+#            print success
 #            print R
 #            R = R%(2*sp.pi)
 #            print R
 #            title = 'Frequency is: %1.4f, Phase is: %1.4f' %(R[0],R[1])
 #            pylab.suptitle(title)
-            pylab.plot(freqs,fitfunc(R,freqs),label='Ratio_'+str(i),c='g')
+#            pylab.plot(fin,fitfunc(R,fin),label='Ratio_'+str(i),c='g')
 #            R_avg = [0.1354,2.341]
 #            pylab.plot(fin,fitfunc(R_avg,fin),label='test_'+str(i),c='b')
-            pylab.scatter(freqs,R_data[i,:],label='Ratio_'+str(i),s=1,c='r',edgecolors='none')
+#            pylab.scatter(freqs,R_data[i,:],label='Ratio_'+str(i),s=1,c='r',edgecolors='none')
             
-            print 'Differential phase frequency for scan ',i,' is:',R[0]
-            print 'Differential phase "phase" for scan ',i,' is:',R[1]
-            R_fin[0,i] = R[0]
-            R_fin[1,i] = R[1]
-        
-#        np.savetxt('3C286_diff_phase_slope.txt',R_fin[0],delimiter = ' ')
-#        np.savetxt('3C286_diff_phase_intercept.txt',R_fin[1],delimiter = ' ')    
+#            print 'Differential phase frequency for scan ',i,' is:',R[0]
+#            print 'Differential phase "phase" for scan ',i,' is:',R[1]
+#            R_fin[0,i] = R[0]
+#            R_fin[1,i] = R[1]
+    
 #        freq_avg = ma.mean(R_fin[0,:])
 #        phase_avg = ma.mean(R_fin[1,:])
 #        freq_avg = 0.1354
@@ -338,12 +385,14 @@ class MuellerGen(object) :
 #            print 'The difference in phase for scan ',i,' between U/M and V/M is:',UT[2]-VT[2]
 #            print '==================================='
         
-#        leg = pylab.legend(fancybox='True')
-#        leg.get_frame().set_alpha(0.25)    
-        pylab.xlim(freqs[0],freqs[-1])
+        leg = pylab.legend(fancybox='True')
+        leg.get_frame().set_alpha(0.25)    
+        pylab.xlim(freqs[0,0],freqs[0,-1])
 #        pylab.ylim(-1,1)
 #        pylab.legend()
-        pylab.savefig('point_source_differential_phase_compare.png')
+        pylab.xlabel('Frequency (MHz)')
+        pylab.ylabel('Temperature (Kelvin)')
+        pylab.savefig('test.png')
         pylab.clf()
         
 #        np.savetxt('mueller_params_calc.txt',p_val_out,delimiter = ' ')
