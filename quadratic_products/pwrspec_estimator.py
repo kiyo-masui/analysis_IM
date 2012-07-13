0e2b32b695369d312519e88005d2cb27f2b48d24
Eric Switzer <eric.switzer@gmail.com>
2012-07-13 16:41:42 -0400

    improved memory mgmt in pwrspec est.

--- quadratic_products/pwrspec_estimator.py
+++ quadratic_products/pwrspec_estimator.py
@@ -12,24 +12,87 @@ import scipy.special
 import math
 from core import algebra
 import copy
+import gc
 from utils import fftutil
 from utils import binning
 
 
 def cross_power_est(arr1, arr2, weight1, weight2,
                     window="blackman", nonorm=False):
-    """Calculate the radially average cross-power spectrum of a two nD fields.
+    """Calculate the cross-power spectrum of a two nD fields.
 
     The arrays must be identical and have the same length (physically
     and in pixel number) along each axis.
 
-    Parameters
-    ----------
-    arr1, arr2: np.ndarray
-        The cubes to calculate the cross-power spectrum of. If arr2 is
-        None then return the standard (auto-)power spectrum.
-    window: boolean
-        Apply an additional named window
+    inputs are clobbered to save memory
+    """
+
+    # make the axes
+    ndim = arr1.ndim
+    k_axes = tuple(["k_" + axis_name for axis_name in arr1.axes])
+    info = {'axes': k_axes, 'type': 'vect'}
+    width = np.zeros(ndim)
+    for axis_index in range(ndim):
+        n_axis = arr1.shape[axis_index]
+        axis_name = arr1.axes[axis_index]
+        axis_vector = arr1.get_axis(axis_name)
+        delta_axis = abs(axis_vector[1] - axis_vector[0])
+        width[axis_index] = delta_axis
+
+        k_axis = np.fft.fftshift(np.fft.fftfreq(n_axis, d=delta_axis))
+        k_axis *= 2. * math.pi
+        delta_k_axis = abs(k_axis[1] - k_axis[0])
+
+        k_name = k_axes[axis_index]
+        info[k_name + "_delta"] = delta_k_axis
+        info[k_name + "_centre"] = 0.
+        #print k_axis
+        #print k_name, n_axis, delta_axis
+
+    if window:
+        window_function = fftutil.window_nd(arr1.shape, name=window)
+        weight1 *= window_function
+        weight2 *= window_function
+        del window_function
+
+    arr1 *= weight1
+    arr2 *= weight2
+
+    # correct for the weighting
+    fisher_diagonal = np.sum(weight1 * weight2)
+
+    fft_arr1 = np.fft.fftshift(np.fft.fftn(arr1))
+
+    fft_arr2 = np.fft.fftshift(np.fft.fftn(arr2))
+
+    fft_arr1 *= fft_arr2.conj()
+    xspec = fft_arr1.real
+    del fft_arr1, fft_arr2
+    gc.collect()
+
+    xspec /= fisher_diagonal
+
+    # make the axes
+    xspec_arr = algebra.make_vect(xspec, axis_names=k_axes)
+
+    xspec_arr.info = info
+    #print xspec_arr.get_axis("k_dec")
+
+    if not nonorm:
+        xspec_arr *= width.prod()
+
+    return xspec_arr
+
+
+def cross_power_est_highmem(arr1, arr2, weight1, weight2,
+                    window="blackman", nonorm=False):
+    """Calculate the cross-power spectrum of a two nD fields.
+
+    The arrays must be identical and have the same length (physically
+    and in pixel number) along each axis.
+
+    Same goal as above without the emphasis on saving memory.
+    This is the "tried and true" legacy function.
     """
     if window:
         window_function = fftutil.window_nd(arr1.shape, name=window)
@@ -159,17 +222,9 @@ def calculate_xspec(cube1, cube2, weight1, weight2,
 
     print "finding the signal power spectrum"
     pwrspec3d_signal = cross_power_est(cube1, cube2, weight1, weight2,
-                               window=window)
+                                       window=window)
 
     radius_arr = binning.radius_array(pwrspec3d_signal)
-
-    # find the k_perp by not including k_nu in the distance
-    radius_arr_perp = binning.radius_array(pwrspec3d_signal,
-                                           zero_axes=[0])
-
-    # find the k_perp by not including k_RA,Dec in the distance
-    radius_arr_parallel = binning.radius_array(pwrspec3d_signal,
-                                               zero_axes=[1, 2])
 
     if bins is None:
         bins = binning.suggest_bins(pwrspec3d_signal,
@@ -183,7 +238,22 @@ def calculate_xspec(cube1, cube2, weight1, weight2,
         pwrspec3d_signal = make_unitless(pwrspec3d_signal,
                                          radius_arr=radius_arr)
 
+    print "calculating the 1D histogram"
+    counts_histo, binavg = binning.bin_an_array(pwrspec3d_signal, bins,
+                                                radius_arr=radius_arr)
+
+    del radius_arr
+    gc.collect()
+
     print "calculating the 2D histogram"
+    # find the k_perp by not including k_nu in the distance
+    radius_arr_perp = binning.radius_array(pwrspec3d_signal,
+                                           zero_axes=[0])
+
+    # find the k_perp by not including k_RA,Dec in the distance
+    radius_arr_parallel = binning.radius_array(pwrspec3d_signal,
+                                               zero_axes=[1, 2])
+
     # TODO: do better independent binning; for now:
     bins_x = copy.deepcopy(bins)
     bins_y = copy.deepcopy(bins)
@@ -192,9 +262,9 @@ def calculate_xspec(cube1, cube2, weight1, weight2,
                                                          radius_arr_parallel,
                                                          bins_x, bins_y)
 
-    print "calculating the 1D histogram"
-    counts_histo, binavg = binning.bin_an_array(pwrspec3d_signal, bins,
-                                                radius_arr=radius_arr)
+    del radius_arr_perp
+    del radius_arr_parallel
+    gc.collect()
 
     bin_left_x, bin_center_x, bin_right_x = binning.bin_edges(bins_x,
                                                               log=logbins)
