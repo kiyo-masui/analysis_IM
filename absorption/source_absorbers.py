4728988106ec88034ac0b772872e5f23c7b9bd2a
Tabitha Voytek <tcv@prawn.cita.utoronto.ca>
2013-01-23 15:41:49 -0500

    Edited the 3C286 stacking code

--- absorption/source_absorbers.py
+++ absorption/source_absorbers.py
@@ -5,6 +5,7 @@ from scipy.optimize import *
 import scipy as sp
 import numpy.ma as ma
 import numpy as np
+import pylab 
 
 from kiyopy import parse_ini
 import kiyopy.utils
@@ -16,6 +17,7 @@ params_init = {
 	       "file_middles" : ("GBTdata",),
                "input_end" : ".raw.acs.fits",
                "output_root" : "absorbers",
+               "output_end" : ".txt",
 	       "scans" : (),
                "IFs" : (),
                }
@@ -36,19 +38,130 @@ class SourceAbsorb(object):
                                prefix=prefix)
         output_root = params['output_root']
         output_end = params['output_end']
-        
+       
+        self.file_num = len(params['file_middles'])
 
+        sample_file = params['file_middles'][0]
+        input_sample = (params['input_root']+sample_file+params['input_end'])
+        Reader = core.fitsGBT.Reader(input_sample)
+        off_sample = Reader.read(1,0,force_tuple=True)
+        for Data in off_sample:
+            freq_len = Data.dims[3]        
+        
+        On_tot = []
+        Off_tot = [] 
+        RAs_on = []
+        DECs_on = []
+        RAs_off = []
+        DECs_off = []
+                        
         for file_middle in params['file_middles']:
             input_fname = (params['input_root']+file_middle+
                            params['input_end'])
             Reader = core.fitsGBT.Reader(input_fname)
 	    n_IFs = len(Reader.IF_set)
             n_scans = len(Reader.scan_set)
-            OnBlocks = Reader.read(0,n_scans,2),0,force_tuple=True)
-            OffBlocks = Reader.read(1,n_scans,2),0,force_tuple=True)
-            Blocks = Reader.read(params['scans'], params['IFs'],
-                                 force_tuple=True)
+            if n_scans == 2:
+	        OnBlocks = Reader.read(range(0,n_scans,2),0,force_tuple=True)
+        	OffBlocks = Reader.read(range(1,n_scans,2),0,force_tuple=True)
+	        Blocks = Reader.read(params['scans'], params['IFs'],
+        	                     force_tuple=True)
+            for Data in OnBlocks:
+                freq_len = Data.dims[3]
+                time_len = Data.dims[0] 
+                Data.calc_freq() 
+                freq_val = Data.freq
+                freq_val = freq_val/1000000 
+                Full_date = Data.field['DATE-OBS']
+#                p#rint Full_date
+                Az = Data.field['CRVAL2'] 
+#                print Az 
+                El = Data.field['CRVAL3'] 
+#                print El 
+#                print len(Az),len(El),len(Full_date)
+                RA_data = sp.zeros(len(Az)) 
+                DEC_data = sp.zeros(len(Az))
+                for i in range(0,len(Az)): 
+                    RA_data[i], DEC_data[i] = utils.azel2radecGBT(Az[i],El[i],Full_date[i])
+#                    print RA_data[i], DEC_data[i]
+#                print ma.mean(RA_data)
+                off_data = Data.data[:,0,1,:]
+                off_med = ma.median(off_data,axis=0)# This is cal off, not src off
+                On_tot.append(off_med)
+                RAs_on.append(ma.mean(RA_data))
+                DECs_on.append(ma.mean(DEC_data))
+ 
+	    for Data in OffBlocks:
+	        freq_len = Data.dims[3]
+                time_len = Data.dims[0]
+                Data.calc_freq()
+                freq_val = Data.freq
+                freq_val = freq_val/1000000
+                Full_date = Data.field['DATE-OBS']
+#                p#rint Full_date
+                Az = Data.field['CRVAL2']
+#                print Az
+                El = Data.field['CRVAL3']
+#                print El
+#                print len(Az),len(El),len(Full_date)
+                RA_data = sp.zeros(len(Az))
+                DEC_data = sp.zeros(len(Az))
+                for i in range(0,len(Az)):
+                    RA_data[i], DEC_data[i] = utils.azel2radecGBT(Az[i],El[i],Full_date[i])
+#                    print RA_data[i], DEC_data[i]
+                
+                off_data = Data.data[:,0,1,:]
+                off_med = ma.median(off_data,axis=0)# This is cal off, not src off
+                Off_tot.append(off_med)
+                RAs_off.append(ma.mean(RA_data))
+                DECs_off.append(ma.mean(DEC_data))
+                
+#        print np.shape(Data_tot)        
+#        print RAs
+#        print DECs
+#        Data_tot = np.array(Data_tot)
 
-            
-            m =0
-            
+        offlarge = []
+        offsmall = []
+        for i in range(0,len(Off_tot)):
+            if RAs_off[i]>203.0:
+                offlarge.append(Off_tot[i])
+            else:
+                offsmall.append(Off_tot[i])
+        
+        On_med = ma.median(On_tot,axis=0)
+        Off_med_lrg = ma.median(offlarge,axis=0)
+        Off_med_sml = ma.median(offsmall,axis=0)
+        
+        min =0
+        max = len(freq_val)
+        for i in range(0,len(freq_val)):
+            if freq_val[i]>=835.0:
+                min = i
+            if freq_val[i]>=845.0:
+                max = i
+        
+#        print freq_val[min],freq_val[max]
+        pylab.scatter(freq_val[max:min],On_med[max:min],c='b',edgecolor='b',s=3,label='On Src')
+        pylab.ylim(50,60)
+        pylab.ylabel('Temperature (K)')
+        pylab.xlabel('Frequency (MHz)')
+        pylab.savefig('absorb_stack_3C286')
+        pylab.clf()
+
+        pylab.scatter(freq_val[max:min],Off_med_lrg[max:min],c='g',edgecolor='g',s=3,label='Off Above')
+        pylab.scatter(freq_val[max:min],Off_med_sml[max:min],c='r',edgecolor='r',s=3,label='Off Below')
+        pylab.ylim(15,25)
+        pylab.ylabel('Temperature (K)')
+        pylab.xlabel('Frequency (MHz)')
+        pylab.savefig('absorb_stack_off_src')
+        pylab.clf()
+
+       
+#        np.savetxt('3C286_absorb_data.txt',stack_data,delimiter=' ')
+
+
+         
+if __name__ == "__main__":
+    import sys
+    SourceAbsorb(str(sys.argv[1])).execute()  
