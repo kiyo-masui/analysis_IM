cf7e4f6bd11e5d4e78460443848a5d10bc090c35
mufma <mufma@mail.ru>
2013-06-25 14:58:46 -0400

    changed entries in subprocess.Popen

--- f_NL_project/shelve_pipe.py
+++ f_NL_project/shelve_pipe.py
@@ -4,10 +4,12 @@ import shelve_outplot as so
 # Initialize settings
 shelve_calc = False
 plotter = True
+manager_path = '/cita/h/home-2/mufma/code/analysis_IM/pipeline/manager.py'
+ini_path = 'input/mm/autopower/kappa_dm_sim.ini'
 
 # Produce shelve file from the algebra.npy file
 if shelve_calc == True:
-    subprocess.Popen(['python', '/cita/h/home-2/mufma/code/analysis_IM/pipeline/manager.py', 'input/mm/autopower/kappa_dm_sim.ini'])
+    subprocess.Popen(['python', manager_path, ini_path])
 
 # Set up maps for reading
 cross_map = '/mnt/raid-project/gmrt/mufma/goblot_pwrspec/cross_pwr_kap_del.shelve'
