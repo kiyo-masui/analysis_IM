0137936143ca224a5c185f722b811fbe1e652cac
mufma <mufma@mail.ru>
2013-06-25 15:33:15 -0400

    Codes for producing algebra boxes

---
+++ f_NL_project/hdf5_pipe.py
@@ -0,0 +1,48 @@
+import hdf5_to_algebra as ha
+
+# Initialize JD settings
+get_JD_algebra = True
+JD_redshift = [0.800, 0.900, 1.000, 1.100, 1.200, 1.300, 1.400, 1.500,
+               1.600, 1.700, 1.800, 1.900, 2.000, 2.100, 2.200, 2.300,
+               2.400, 2.500, 2.700, 3.000, 3.500, 4.000, 4.500, 5.000,
+               6.000, 7.000, 8.000, 9.000, 10.000]
+index = [0,1,2,3,4,5,6,7]
+JD_bins = 512
+JD_path = "/mnt/raid-project/gmrt/mufma/JD_h5py_catalogs/"
+JD_file = "%.3fhalo_catalog000%d.hdf5"
+group_1 = 'T_b21'
+subgroup_1 = 'T_b21'
+group_2 = 'Galaxies_number'
+subgroup_2 = 'Galaxies_number'
+JD_directory = "/mnt/raid-project/gmrt/mufma/JD_algebra_deltas_%d/"
+JD_savefile_1 = 'T_b21_%dind%.3fred.npy'
+JD_savefile_2 = 'Galaxies_%dind%.3f.npy'
+
+# Initialize JOE settings
+get_algebra = False
+redshift = [0.042,0.086,0.130,0.220,0.267,0.316,0.416,0.468,0.523,0.636,
+            0.696,0.758,0.889,0.958,1.030,1.185,1.267,1.353,1.538,1.638,
+            1.742,1.969,2.092,2.222,2.505,2.660,2.825]
+path = "/mnt/raid-project/gmrt/mufma/h5py_catalogs/"
+folder_file = "simulation%d/%.3fhalo_catalog.hdf5"
+num_bins = 161
+catalogs = [range(1,66),range(67,77),range(78,101)]
+group = 'Halo_Masses'
+subgroup = 'Halo_Masses'
+directory = "/mnt/raid-project/gmrt/mufma/algebra_deltas_%d/"
+savefile  = 'dm_%dsim%.3fred.npy'
+
+# Convert h5py catalogs to the algebra_npy files
+if get_algebra == True:
+    for list in catalogs:
+        for num in list:
+            for red in redshift:
+                ha.converter(path + folder_file%(num,red), num_bins, group,
+                             subgroup, directory, savefile%(num,red))
+            print "finished processing catalog %d"%nums
+
+if get_JD_algebra == True:
+    for red in redshift:
+        for ind in index:
+            ha.converter(JD_path + JD_file%(red,ind), JD_bins, JD_group,
+                         JD_subgroup, JD_directory, JD_savefile%(red,ind))
---
+++ f_NL_project/hdf5_to_algebra.py
@@ -0,0 +1,77 @@
+import h5py
+from core import algebra
+from utils import binning
+import numpy as np
+
+def converter(catalog, num_bins, group, subgroup, directory, savefile):
+    """
+    Convert h5py catalog to binned algebra map
+    """
+    file = h5py.File(catalog, 'r')
+    # reading data from h5py
+    posx = file['Positions']['x'][:]
+    posy = file['Positions']['y'][:]
+    posz = file['Positions']['z'][:]
+    weights = file[group][subgroup][:]
+    file.close()
+    # creating info for binning function
+    xedges = np.linspace(0, posx.max(), num_bins)
+    yedges = np.linspace(0, posy.max(), num_bins)
+    zedges = np.linspace(0, posz.max(), num_bins)
+    sample = np.zeros((len(posx), 4))
+    sample[:, 0] = posx
+    sample[:, 1] = posy
+    sample[:, 2] = posz
+    sample[:, 3] = weights
+    # calling binning function
+    binned_map = binning.histogram3d_weights(sample, xedges, yedges, zedges)
+    # prepare deltas
+    binned_map = (binned_map - binned_map.mean())/(binned_map.mean())
+    # info for pipeline
+    x_delta = posx.max()/num_bins
+    y_delta = posy.max()/num_bins
+    z_delta = posz.max()/num_bins
+    x_centre = posx.max()/2.
+    y_centre = posy.max()/2.
+    z_centre = posz.max()/2.
+    info = {'ra_delta': x_delta,
+            'dec_delta': y_delta,
+            'dec_centre': y_centre,
+            'axes': ('freq', 'ra', 'dec'),
+            'ra_centre': x_centre,
+            'freq_centre': z_centre,
+            'freq_delta': z_delta,
+            'type': 'vect'}
+    # save data for pipeline in manager.py
+    map = algebra.make_vect(binned_map, axis_names=('ra', 'dec', 'freq'))
+    map.info = info
+    save_file = open(directory%(num_bins-1) + savefile, "w")
+    algebra.save(save_file, map)
+    save_file.close()
+
+def convert_npy(directory):
+    file = np.load(directory)
+    x_delta = 0.62890625
+    y_delta = 0.62890625
+    z_delta = 0.62890625
+    x_centre = 161.0
+    y_centre = 161.0
+    z_centre = 161.0
+    info = {'ra_delta': x_delta,
+            'dec_delta': y_delta,
+            'dec_centre': y_centre,
+            'axes': ('freq', 'ra', 'dec'),
+            'ra_centre': x_centre,
+            'freq_centre': z_centre,
+            'freq_delta': z_delta,
+            'type': 'vect'}
+    box = algebra.make_vect(file, axis_names=('ra', 'dec', 'freq'))
+    box.info = info
+    directory = "/mnt/raid-project/gmrt/mufma/"
+    savefile = "kappa512.npy"
+    save_file = open(directory + savefile, "w")
+    algebra.save(save_file, box)
+    save_file.close()
+
+
+
