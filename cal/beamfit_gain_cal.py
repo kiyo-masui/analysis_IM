c073c08cc7b00a4fcec96cf70c3ef4d24989aacd
Tabitha Voytek <tvoytek@fourier.gb.nrao.edu>
2013-07-23 22:03:32 -0400

    Working on a Joint calibration with both the beam fit and the flux cal.

--- cal/beamfit_gain_cal.py
+++ cal/beamfit_gain_cal.py
@@ -2,8 +2,10 @@ import os
 
 import numpy as np
 import numpy.ma as ma
+import scipy as sp
 import matplotlib.pyplot as plt
 from scipy import optimize
+from scipy.optimize import *
 import ephem
 from numpy import ma
 import matplotlib.animation as animation
@@ -15,17 +17,127 @@ from map import pol_beam
 from utils import misc
 import cal.source
 from cal import beam_fit
-from cal import flux_diff_gain_gen_beamfit
+#from cal import flux_diff_gain_gen_beamfit
 
+def calcGain(OnData,OffData,file_num,freq_len,src,beamwidth):
+    """ Perform gain calibration on dataset.
 
-data_root = 'path/to/data/'
+    """
+    def peval(p,data):
+        d = data
+        XG = p[0]
+        YG = p[1]
+        act = sp.zeros(len(data)*4)
+        for i in range(0,len(act),4):
+            act[i] = XG*d[i/4,0]
+            act[i+1] = 0
+            act[i+2] = 0
+            act[i+3] =YG*d[(i+3)/4,3]
+        return act
+
+    def residuals(p,errors,freq_val,src,theta,data,width,file_num):
+
+        wavelength = 300.0/freq_val
+        BW = width*sp.pi/180.
+        JtoK = (sp.pi*wavelength**2)/(8*1380.648*BW**2)
+        Jsrc_name = ['3C286','3C48','3C67','3C147','3C295']
+        Jsrc_val = [19.74748409*pow((750.0/freq_val),0.49899785),
+                    25.15445092*pow((750.0/freq_val),0.75578842),
+                    4.56303633*pow((750.0/freq_val),0.59237327),
+                    31.32846821*pow((750.0/freq_val),0.52113534),
+                    34.11187767*pow((750.0/freq_val),0.62009421)]
+        for i in range(0,len(Jsrc_name)):
+            if Jsrc_name[i]==src:
+                src_ind = i
+        PAsrc = [33.*sp.pi/180.,0.,0.,0.,0.,0.]
+        Psrc = [0.07,0.,0.,0.,0.]
+        Isrc = Jsrc_val[src_ind]*JtoK
+        Qsrc = Isrc*Psrc[src_ind]*sp.cos(2*PAsrc[src_ind])
+        Usrc = Isrc*Psrc[src_ind]*sp.sin(2*PAsrc[src_ind])
+        Vsrc = 0
+        XXsrc0 = Isrc-Qsrc
+        YYsrc0 = Isrc+Qsrc
+        expec =sp.zeros(4*file_num)
+        for i in range(0,len(source),4):
+            expec[i] = (0.5*(1+sp.cos(2*theta[i]))*XXsrc0-sp.sin(2*theta[i])*Usrc+0.5*(1-sp.cos(2*theta[i]))*YYsrc0)
+            expec[i+1] = 0
+            expec[i+2] = 0
+            expec[i+3] = (0.5*(1-sp.cos(2*theta[i]))*XXsrc0+sp.sin(2*theta[i])*Usrc+0.5*(1+sp.cos(2*theta[i]))*YYsrc0)
+        err = (expec-peval(p,data))/errors
+        return err
+###################################################
+# Setting labels for indices for later
+    XX_ind = 0
+    YY_ind = 3
+    XY_ind = 1
+    YX_ind = 2
+
+    S_med_src = sp.zeros((file_num,4,freq_len))
+    S_med = sp.zeros((file_num,4,freq_len))
+
+    PA_on = []
+    m=0
+    for Data in OnData:
+        S_med_src[m,0,:] = ma.median(Data.data[:,XX_ind,:],axis=0)
+        S_med_src[m,1,:] = ma.median(Data.data[:,XY_ind,:],axis=0)
+        S_med_src[m,2,:] = ma.median(Data.data[:,YX_ind,:],axis=0)
+        S_med_src[m,3,:] = ma.median(Data.data[:,YY_ind,:],axis=0)
+        Data.calc_PA()
+        for i in range(0,4):
+            PA_on.append(ma.mean(Data.PA))
+        Data.calc_freq()
+        freq_val = Data.freq/1e6
+        m+=1
+
+    PA_off = []
+    m=0
+    for Data in OffData:
+        S_med[m,0,:] = ma.median(Data.data[:,XX_ind,:],axis=0)
+        S_med[m,1,:] = ma.median(Data.data[:,XY_ind,:],axis=0)
+        S_med[m,2,:] = ma.median(Data.data[:,YX_ind,:],axis=0)
+        S_med[m,3,:] = ma.median(Data.data[:,YY_ind,:],axis=0)
+        Data.calc_PA()
+        for i in range(0,4):
+            PA_off.append(ma.mean(Data.PA))
+        m+=1
+
+    S_data = sp.zeros((file_num,4,freq_len))
+    for i in range(0,len(S_med)):
+        S_data[i,0,:] = S_med_src[i,0,:]-S_med[i,0,:]
+        S_data[i,1,:] = S_med_src[i,1,:]-S_med[i,1,:]
+        S_data[i,2,:] = S_med_src[i,2,:]-S_med[i,2,:]
+        S_data[i,3,:] = S_med_src[i,3,:]-S_med[i,3,:]
+#There are 2 parameters for this version p[0] is XX gain and p[1] is YY gain. 
+    p0 = [1,1] # guessed preliminary values
+    error = sp.ones(4*file_num)
+    #Note that error can be used to weight the equations if not all set to one.
+
+    p_val_out = sp.zeros((freq_len, 3))
+    for f in range(0,freq_len):
+        plsq = leastsq(residuals,p0,args=(error,freq_val[f],src,PA_on,S_data[:,:,f],beamwidth[f],file_num),full_output=0, maxfev=5000)
+        pval = plsq[0] # this is the 1-d array of results0
+
+        p_val_out[f,0] = freq_val[f]
+        p_val_out[f,1] = pval[0]
+        p_val_out[f,2] = pval[1]
+
+    JtoK = sp.pi*(300./freq_val)**2/(8*1380.648*(beamwidth*sp.pi/180.)**2)
+
+#    out_path = output_root+sess+'_diff_gain_calc'+output_end
+#    np.savetxt(out_path,p_val_out,delimiter = ' ')
+    return p_val_out,JtoK
+
+data_root = '/home/scratch/kmasui/converted_fits/GBT12A_418/'
 end = '.fits'
 
-source = '3C286'
+source = '3C295'
+gain_source = '3C295'
 
-beam_cal_files = [file_middles]
+#beam_cal_files = ['21_3C286_track_'+str(ii) for ii in range(18,26)]
+beam_cal_files = ['22_3C295_track_'+str(ii) for ii in range(59,67)]
 
-gain_cal_files = [file_middles]
+gain_cal_files = ['22_3C295_onoff_76-77','22_3C295_onoff_78-79']
+#gain_cal_files = ['22_3C147_onoff_50-51','22_3C147_onoff_52-53','22_3C147_onoff_6-7','22_3C147_onoff_8-9']
 
 beam_cal_Blocks = []
 for fname in beam_cal_files:
@@ -33,7 +145,7 @@ for fname in beam_cal_files:
     fpath = data_root + fname + end
     Reader = fitsGBT.Reader(fpath)
     Data = Reader.read(0,0)
-    cal_Blocks.append(Data)
+    beam_cal_Blocks.append(Data)
 
 for Data in beam_cal_Blocks:
     # Preprocess.
@@ -76,12 +188,13 @@ for Data in gain_cal_OffBlocks:
     #rebin_freq.rebin(Data, 16, True, True)
     rebin_freq.rebin(Data, 16, True, True)
     #combine_cal.combine(Data, (0.5, 0.5), False, True)
+
     combine_cal.combine(Data, (0., 1.), False, True)
     #rebin_time.rebin(Data, 4)
 
 Data.calc_freq()
 
-BeamCalData = beam_fit.FormattedData(beam_cal_Blocks)
+BeamData = beam_fit.FormattedData(beam_cal_Blocks)
 #GainCalOnData = beam_fit.FormattedData(gain_cal_OnBlocks)
 #GainCalOffData = beam_fit.FormattedData(gain_cal_OffBlocks)
 
@@ -99,14 +212,130 @@ HermiteBasis = pol_beam.HermiteBasis(Data.freq, center_offset, width)
 # Perform the fit.
 beam_params, scan_params, model_data = beam_fit.linear_fit(BeamData, HermiteBasis,
                                                            S, 3, 2)
-diff_gain_cal_gen_beamfit.calcGain(gain_cal_OnBlocks,gain_cal_OffBlocks,len(gain_cal_files),len(Data.freq),source,width)
+p_val_out,JtoK = calcGain(gain_cal_OnBlocks,gain_cal_OffBlocks,len(gain_cal_files),len(Data.freq),gain_source,width)
+
+#out_path = output_root+sess+'_diff_gain_calc'+output_end
+np.savetxt(source+'_test_cal',p_val_out,delimiter = ' ')
 
 # Make a beam object from the basis funtions and the fit parameters (basis
 # coefficients).
 Beam = pol_beam.LinearBeam(HermiteBasis, beam_params)
 
 # Some plots.
-beam_map = Beam.get_full_beam(100, 1)
+beam_map = Beam.get_full_beam(100, 1.)
+
+freq = Data.freq/1e6
+plt.figure()
+plt.plot(freq,center_offset[:,0])
+plt.plot(freq,center_offset[:,1])
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Pointing Offset (Degrees)')
+plt.legend(('Az','El'))
+plt.xlim(700,900)
+plt.ylim(-0.05,0.05)
+plt.grid()
+plt.savefig(source+'_pointing_offsets',dpi=300)
+plt.clf()
+
+plt.figure()
+plt.plot(freq,width)
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Beam Width (Degrees)')
+plt.xlim(700,900)
+plt.grid()
+plt.savefig(source+'_beamwidth',dpi=300)
+plt.clf()
+
+plt.figure()
+plt.plot(freq,JtoK)
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Jansky to Kelvin Conversion')
+plt.xlim(700,900)
+plt.grid()
+plt.savefig(source+'_JytoK_convert',dpi=300)
+plt.clf()
+
+n_chan = len(freq)
+for i in range(0,20):
+    f_ind = i*n_chan/20.
+    plt.figure()
+    color_map=1
+    spec_beam_map = beam_map[f_ind,...]
+    n_pol = spec_beam_map.shape[0]
+    n_side = spec_beam_map.shape[1]
+    norm_x = spec_beam_map[0,n_side//2,n_side//2]
+    norm_y = spec_beam_map[3,n_side//2,n_side//2]
+#    print np.where(np.isnan(norm_x))[0],np.where(np.isinf(norm_x))[0],np.where(norm_x==0)[0]
+#    print np.where(np.isnan(norm_y))[0],np.where(np.isinf(norm_y))[0],np.where(norm_y==0)[0]
+    badx = 0
+    bady = 0
+    if norm_x==0:
+        if norm_y==0:
+            norm_x =1.
+        else:
+            norm_x = norm_y 
+        print 'XX Normalization failed'
+        badx = 1
+    if norm_y==0:
+        if norm_x==0:
+            norm_y = 1.
+        else:
+            norm_y = norm_x
+        bady = 1
+        print 'YY Normalization failed'
+    norm_cross = np.sqrt(norm_x*norm_y)
+    spec_beam_map[0] /=norm_x
+    spec_beam_map[3] /=norm_y
+    spec_beam_map[[1,2]]/=norm_cross
+    neg = spec_beam_map<0
+    spec_beam_map = abs(spec_beam_map)**color_map
+    spec_beam_map.shape = (n_pol*n_side,n_side)
+#    spec_beam_map.shape = (n_pol*1.,1.)
+    plt.imshow(spec_beam_map.T)
+#    pol_beam.plot_beam_map(beam_map[f_ind,...],color_map=0.5)
+    cbar = plt.colorbar()
+    if badx==0:
+        if bady==0:           
+           cbar.set_label(r"Beam Center Normalized Intensity")
+        elif bady==0:
+           cbar.set_label(r"Beam Center Normalized Intensity (Y-norm failed)")
+    elif badx==1:
+        if bady==0:
+           cbar.set_label(r"Beam Center Normalized Intensity (X-norm failed)")
+        elif bady==1:
+           cbar.set_label(r"Beam Center Un-Normalized Intensity")
+    plt.xlabel(r"XX'XY'YX'YY, Azimuth")
+    plt.ylabel(r"Elevation")
+#    plt.xlabel(r"XX'XY'YX'YY, Azimuth (degrees)")
+#    plt.ylabel(r"Elevation (degrees)")
+    plt.title("Beam Patterns for %0.1f MHz" %freq[f_ind])
+    raw_title = source+'_'+str(int(freq[f_ind]))+'_MHz_beam_pattern'
+    plt.savefig(raw_title,dpi=300)
+    plt.clf()
+
+plt.figure()
+Tsys_Kel = Tsys
+Tsys_Kel[:,0] = Tsys[:,0]*p_val_out[:,1]
+Tsys_Kel[:,1] = Tsys[:,1]*p_val_out[:,2]
+plt.plot(freq,Tsys_Kel[:,0])
+plt.plot(freq,Tsys_Kel[:,1])
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Tsys (Kelvin)')
+plt.grid()
+plt.legend(('XX','YY'))
+plt.savefig(gain_source+'_Tsys_Kelvin',dpi=300)
+plt.clf()
+
+plt.figure()
+plt.plot(freq,p_val_out[:,1])
+plt.plot(freq,p_val_out[:,2])
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Tcal (Kelvin)')
+plt.grid()
+plt.legend(('XX','YY'))
+plt.savefig(gain_source+'_Tcal_Kelvin',dpi=300)
+plt.clf()
+
 
 #plt.figure()
 #this_data, this_weight = BeamData.get_data_weight_chan(35)
--- cal/flux_diff_gain_cal_gen_beamfit.py
+++ cal/flux_diff_gain_cal_gen_beamfit.py
@@ -14,31 +14,208 @@ import numpy.ma as ma
 import numpy as np
 
 from utils import misc
+import source
+import matplotlib.pyplot as plt
+from scipy import optimize, linalg
+
+class FormattedData(object):
+    """All data that goes into a fit.
+
+    This class holds GBT SDfits data in a format that makes it easy for
+    fitting.
+    
+    Parameters
+    ----------
+    Blocks : list of DataBlock objects.
+        Data to be used for fit.
+    """
+
+    def __init__(self, Blocks):
+        self._data = self.preprocess_data(Blocks)
+        # Figure out all dimensions.
+        self.n_chan = self._data[0]['data'].shape[0]
+        self.n_pol = self._data[0]['data'].shape[1]
+        n_time = 0
+        for data in self._data:
+            s = data['data'].shape
+            n_time += s[2]
+            if s[0] != self.n_chan or s[1] != self.n_pol:
+                msg = "Data blocks have conflicting dimensions."
+                raise ValueError(msg)
+        self.n_time = n_time
+        self.n_scan = len(self._data)
+
+    def get_data_weight_chan(self, chan_ind):
+        """Get all data and weights for a single channel."""
+
+        data_arr = np.empty((self.n_pol, self.n_time), dtype=np.float64)
+        weights_arr = np.empty((self.n_pol, self.n_time), dtype=np.float64)
+        for data, start, end in self.iterate_scans():
+            this_chan_data = data['data'][chan_ind,:,:]
+            this_chan_weights = data['weight'][chan_ind,:,:]
+            data_arr[:,start:end] = this_chan_data
+            weights_arr[:,start:end] = this_chan_weights
+        return data_arr, weights_arr
+
+    def get_all_data_weight(self):
+        data_arr = np.empty((self.n_chan, self.n_pol, self.n_time),
+                            dtype=np.float64)
+        weights_arr = np.empty((self.n_chan, self.n_pol, self.n_time),
+                               dtype=np.float64)
+        for data, start, end in self.iterate_scans():
+            this_chan_data = data['data']
+            this_chan_weights = data['weight']
+            data_arr[:,:,start:end] = this_chan_data
+            weights_arr[:,:,start:end] = this_chan_weights
+        return data_arr, weights_arr
+
+    def get_all_field(self, field):
+        """Get all of a specific data field."""
+
+        format = self._data[0][field].dtype
+        out = np.empty(self.n_time, dtype=format)
+        for data, start, end in self.iterate_scans():
+            this_field = data[field]
+            out[start:end] = this_field
+        return out
+
+    def generate_scan_basis_polys(self, order):
+        """Generate basis time polynomials for each scan up to order.
+        
+        Generate basis polynomials as a function of time for each scan 
+        up to the passed maximum order.
+
+        Parameters
+        ----------
+        order : integer
+            Maximum order of the polynomials.
+
+        Returns
+        -------
+        polys : ndarray of shape (n_scan, `order`, n_time)
+        """
+
+        polys = np.zeros((self.n_scan, order, self.n_time),
+                         dtype=np.float64)
+        scan_ind = 0
+        for data, start, end in self.iterate_scans():
+            time = data['time']
+            this_poly = misc.ortho_poly(time, order)
+            polys[scan_ind,:,start:end] = this_poly
+            scan_ind +=1
+        return polys
+
+    def iterate_scans(self):
+        """Returns an iterator over the data scans, with start and end time
+        indeces."""
+
+        class iterator(object):
+
+            def __init__(self_itr):
+                self_itr.scan = 0
+                self_itr.time_ind = 0
+                self_itr._data = self._data
+
+            def __iter__(self_itr):
+                return self_itr
+
+            def next(self_itr):
+                if len(self_itr._data) <= self_itr.scan:
+                    raise StopIteration()
+                start = self_itr.time_ind
+                data = self._data[self_itr.scan]
+                self_itr.scan += 1
+                end = start + data['data'].shape[2]
+                self_itr.time_ind = end
+                return data, start, end
+
+        return iterator()
+
+    def preprocess_data(self, Blocks):
+        """Puts data into convienient format and calculates weights."""
+
+        # Where we will store formated data.
+        data_list = []
+        for Data in Blocks:
+            # First do some basic checks on data format.
+            if not tuple(Data.field['CRVAL4']) == (-5, -7, -8, -6):
+                msg = "Expected data polarizations to be XX, XY, YX, YY."
+                raise NotImplementedError(msg)
+            if not Data.data.shape[2] == 1:
+                msg = "Multiple cal states, ignoring all but first."
+                raise Warning(msg)
+
+            # Rearrange data.
+            data = np.swapaxes(Data.data[:,:,0,:], 0, 2)
+            mask = ma.getmaskarray(data).copy()
+            data = data.filled(0)
+            # XX and YY data should all be positive.
+            mask[data[:,[0],:] < 0] = True
+            mask[data[:,[3],:] < 0] = True
+            # If any polarizations are masked, all polarizations should be.
+            mask[np.any(mask, 1)[:,None,:]] = True
+            data[mask] = 0
+
+            # Make the noise weights.
+            weight = data.copy()
+            cross_norm = np.sqrt(weight[:,0,:] * weight[:,3,:])
+            weight[:,[1,2],:] = cross_norm[:,None,:]
+            weight[mask] = 1
+            weight = 1 / weight
+            weight[mask] = 0
+
+            # Get the pointing.
+            Data.calc_pointing()
+            ra = Data.ra
+            dec = Data.dec
+            # Telescope frame.
+            az = Data.field['CRVAL2']
+            el = Data.field['CRVAL3']
+            UT = Data.field['DATE-OBS']
+            # Get the time array.
+            Data.calc_time()
+            time = Data.time
+
+            # Store it all and add to list.
+            this_data = {}
+            this_data['data'] = data
+            this_data['weight'] = weight
+            this_data['ra'] = ra
+            this_data['dec'] = dec
+            this_data['az'] = az
+            this_data['el'] = el
+            this_data['time'] = time
+            this_data['UT'] = UT
+            data_list.append(this_data)
+        return data_list
 
 def calcGain(OnData,OffData,file_num,freq_len,src,beamwidth):
-    def peval(data,p,f):
+    """ Perform gain calibration on dataset.
+
+    """
+    def peval(p,data):
         d = data
         XG = p[0]
         YG = p[1]
-        t = sp.zeros(len(data)*4)
-        for i in range(0,len(t),4):
-            t[i] = XG*d[i/4,0,f]
-            t[i+1] = 0
-            t[i+2] = 0
-            t[i+3] =YG*d[(i+3)/4,3,f]
-        return t 
+        act = sp.zeros(len(data)*4)
+        for i in range(0,len(act),4):
+            act[i] = XG*d[i/4,0]
+            act[i+1] = 0
+            act[i+2] = 0
+            act[i+3] =YG*d[(i+3)/4,3]
+        return act
 
-    def residuals(p,errors, f,freq_val,src,theta,data,width):
+    def residuals(p,errors,freq_val,src,theta,data,width,file_num):
 
-        wavelength = 300.0/freq_val[f]
-        BW = width[f]*180./sp.pi
+        wavelength = 300.0/freq_val
+        BW = width*180./sp.pi
         JtoK = (sp.pi*wavelength**2)/(8*1380.648*BW**2)
 	Jsrc_name = ['3C286','3C48','3C67','3C147','3C295']
-	Jsrc_val = [19.74748409*pow((750.0/freq_val[f]),0.49899785),
-		    25.15445092*pow((750.0/freq_val[f]),0.75578842),
-		    4.56303633*pow((750.0/freq_val[f]),0.59237327),
-		    31.32846821*pow((750.0/freq_val[f]),0.52113534),
-                    34.11187767*pow((750.0/freq_val[f]),0.62009421)]
+	Jsrc_val = [19.74748409*pow((750.0/freq_val),0.49899785),
+		    25.15445092*pow((750.0/freq_val),0.75578842),
+		    4.56303633*pow((750.0/freq_val),0.59237327),
+		    31.32846821*pow((750.0/freq_val),0.52113534),
+                    34.11187767*pow((750.0/freq_val),0.62009421)]
 	for i in range(0,len(Jsrc_name)):
 	    if Jsrc_name[i]==src:
 		src_ind = i
@@ -50,13 +227,13 @@ def calcGain(OnData,OffData,file_num,freq_len,src,beamwidth):
         Vsrc = 0
         XXsrc0 = Isrc-Qsrc
         YYsrc0 = Isrc+Qsrc
-        source = sp.zeros(4*self.file_num)
+        expec =sp.zeros(4*file_num)
         for i in range(0,len(source),4):
-            source[i] = (0.5*(1+sp.cos(2*theta[i]))*XXsrc0-sp.sin(2*theta[i])*Usrc+0.5*(1-sp.cos(2*theta[i]))*YYsrc0)
-            source[i+1] = 0
-            source[i+2] = 0
-            source[i+3] = (0.5*(1-sp.cos(2*theta[i])*XXsrc0+sp.sin(2*theta[i])*Usrc+0.5*(1+sp.cos(2*theta[i])*YYsrc0)
-        err = (source-self.peval(data,p,f))/errors
+            expec[i] = (0.5*(1+sp.cos(2*theta[i]))*XXsrc0-sp.sin(2*theta[i])*Usrc+0.5*(1-sp.cos(2*theta[i]))*YYsrc0)
+            expec[i+1] = 0
+            expec[i+2] = 0
+            expec[i+3] = (0.5*(1-sp.cos(2*theta[i]))*XXsrc0+sp.sin(2*theta[i])*Usrc+0.5*(1+sp.cos(2*theta[i]))*YYsrc0)
+        err = (expec-peval(p,data))/errors
         return err
     
 ###################################################
@@ -77,7 +254,8 @@ def calcGain(OnData,OffData,file_num,freq_len,src,beamwidth):
         S_med_src[m,2,:] = ma.median(Data.data[:,YX_ind,:],axis=0)
         S_med_src[m,3,:] = ma.median(Data.data[:,YY_ind,:],axis=0)
         Data.calc_PA()
-	PA_on.append(ma.mean(Data.PA))
+        for i in range(0,4):
+  	    PA_on.append(ma.mean(Data.PA))
 	Data.calc_freq()
 	freq_val = Data.freq/1e6
         m+=1
@@ -90,7 +268,8 @@ def calcGain(OnData,OffData,file_num,freq_len,src,beamwidth):
         S_med[m,2,:] = ma.median(Data.data[:,YX_ind,:],axis=0)
         S_med[m,3,:] = ma.median(Data.data[:,YY_ind,:],axis=0)
         Data.calc_PA()
-	PA_off.append(ma.mean(Data.PA))
+        for i in range(0,4):
+	    PA_off.append(ma.mean(Data.PA))
         m+=1
  
     S_data = sp.zeros((file_num,4,freq_len))
@@ -106,18 +285,19 @@ def calcGain(OnData,OffData,file_num,freq_len,src,beamwidth):
 
     p_val_out = sp.zeros((freq_len, 3))
     for f in range(0,freq_len):   
-        plsq = leastsq(residuals,p0,args=(error,f,freq_val,src,PA_on,S_data,beamwidth),full_output=0, maxfev=5000)
+        plsq = leastsq(residuals,p0,args=(error,freq_val[f],src,PA_on,S_data[:,:,f],beamwidth[f],file_num),full_output=0, maxfev=5000)
         pval = plsq[0] # this is the 1-d array of results0
 
         p_val_out[f,0] = freq_val[f]
         p_val_out[f,1] = pval[0]
         p_val_out[f,2] = pval[1]
 
-    out_path = output_root+sess+'_diff_gain_calc'+output_end
-    np.savetxt(out_path,p_val_out,delimiter = ' ')
+#    out_path = output_root+sess+'_diff_gain_calc'+output_end
+#    np.savetxt(out_path,p_val_out,delimiter = ' ')
+    return p_val_out
 
 #If this file is run from the command line, execute the main function.
-if __name__ == "__main__":
-    import sys
-    DiffGainGen(str(sys.argv[1])).execute()
+#if __name__ == "__main__":
+#    import sys
+#    DiffGainGen(str(sys.argv[1])).execute()
 
