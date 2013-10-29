df5826a439115355eef957e98b6e0fc76f0e5062
Tabitha Voytek <tcv@prawn.cita.utoronto.ca>
2013-10-29 16:07:32 -0400

    New Absorption work, bug repair in correlate_map and usage edits.

--- absorption/map_analysis.py
+++ absorption/map_analysis.py
@@ -18,11 +18,17 @@ def residuals(params,freq,data):
     return err
 
 directory = sys.argv[1]
+fitting = True
+plotting = False
+filtered = True
+hit_plot = False
+candidate_plot = True
+
 #filename1 = sys.argv[1]
 
 file_base = 'fir_15hr_absorb_clean_map_I_'
 
-file_sets = ('749','768','788','807','827')
+file_sets = ('712','732','749','751','768','771','788','790','807','810','827')
 
 full_data = []
 full_freqs = []
@@ -36,67 +42,177 @@ for i in range(0,len(file_sets)):
     freqs = freqs/1e6
     full_data.append(array)
     full_freqs.append(freqs)
+#    print freqs[0],freqs[-1]
 
-#print shape(full_data)
-#print shape(full_freqs)
+print shape(full_data)
+print shape(full_freqs)
 
-list_freq = zeros(2000)
-list_array = zeros((2000,16,10))
-for i in range(0,2000):
-    if i<400:
-        list_freq[i]=full_freqs[0][399-i]
-        list_array[i,:,:] = full_data[0][399-i]
-    elif i<800:
-        list_freq[i]=full_freqs[1][399-(i-400)]
-        list_array[i,:,:] = full_data[1][799-i]
-    elif i<1200:
-        list_freq[i]=full_freqs[2][399-(i-800)]
-        list_array[i,:,:] = full_data[2][1199-i]
-    elif i<1600:
-        list_freq[i]=full_freqs[3][399-(i-1200)]
-        list_array[i,:,:] = full_data[3][1599-i] 
-    else:
-        list_freq[i]=full_freqs[4][399-(i-1600)]
-        list_array[i,:,:] = full_data[4][1999-i]
+list_freq = zeros((len(full_data),len(full_data[0])))
+list_array = zeros((len(full_data),len(full_data[0]),16,10))
+for i in range(0,len(full_data)):
+    for j in range(0,len(full_data[0])):
+        list_freq[i,j] = full_freqs[i][399-j]
+        list_array[i,j,:,:] = full_data[i][399-j]
 
-fits = zeros((2,16,10))
-for i in range(0,16):
-    for j in range(0,10):
-        data = list_array[:,i,j]
-        params0 = [-0.5,2.5]
-        plsq = opt.leastsq(residuals,params0,args=(list_freq,data),full_output=0,maxfev=5000)
+if fitting:
+    fits = zeros((len(list_freq),2,16,10))
+    for i in range(0,16):
+        for j in range(0,10):
+	    for k in range(0,len(list_freq)):
+                data = list_array[k,:,i,j]
+                params0 = [-0.5,2.5]
+                plsq = opt.leastsq(residuals,params0,args=(list_freq[k],data),full_output=0,maxfev=5000)
 #        print plsq
-        fits[0,i,j] = plsq[0][0]
-        fits[1,i,j] = plsq[0][1]
+                fits[k,0,i,j] = plsq[0][0]
+                fits[k,1,i,j] = plsq[0][1]
 
+    fit_data = zeros((len(list_array),len(list_array[0]),16,10))
+    for i in range(0,16):
+        for j in range(0,10):
+            for k in range(0,len(list_array)):
+                fit_data[k,:,i,j] = function(list_freq[k],fits[k,:,i,j])
 
-plt.subplot(121)
-plt.imshow(fits[0],interpolation='nearest')
-plt.colorbar()
-plt.title('Amplitude')
-plt.subplot(122)
-plt.imshow(fits[1],interpolation='nearest')
-plt.colorbar()
-plt.title('Power')
+if plotting:
+    for i in range(0,len(list_freq)):
+        plt.plot(list_freq[i],list_array[i,:,8,5])
+    #plt.show()
+
+#plt.subplot(121)
+#plt.imshow(fits[0,0],interpolation='nearest')
+#plt.colorbar()
+#plt.title('Amplitude')
+#plt.subplot(122)
+#plt.imshow(fits[0,1],interpolation='nearest')
+#plt.colorbar()
+#plt.title('Power')
 #plt.show()
 #print fits
-plt.clf()
+#plt.clf()
 
-#plt.plot(list_freq)
+    for i in range(0,len(list_freq)):
+        plt.plot(list_freq[i],fit_data[i,:,8,5])
+    #plt.show()
+
+    plt.clf()
+#plt.title('High Resolution Residual Spectra for (15 arcmin)^2 Pixels') 
+    for j in range(0,16):
+        for k in range(0,10):
+            index = j*10+k
+#            index = (k-1)*12+(j-2)
+#            index = (j-2)*8+(k-1)
+            plt.subplot(16,10,index)
+            for i in range(0,len(list_freq)):
+                plt.plot(list_freq[i],list_array[i,:,j,k]-fit_data[i,:,j,k])
+            plt.xlim(700,780)
+            plt.ylim(-0.05,0.05)
+            plt.grid()
+    plt.subplot(16,10,6)
+    plt.title('High Resolution Power Law Subtracted (15 arcmin)^2 Spectra')
+    plt.savefig('absorber_power_law_sub_maps',dpi=300)
+#    plt.show()
+    plt.clf()
+
+    for j in range(0,16):
+        for k in range(0,10):
+            for i in range(0,len(list_freq)):
+                plt.plot(list_freq[i],list_array[i,:,j,k]-fit_data[i,:,j,k])
+            plt.xlabel('Frequency (MHz)')
+            plt.ylabel('Single Spatial Pixel Spectrum Residuals (Kelvin)')
+            plt.ylim(-0.1,0.1)
+            plt.grid()
+            plt.title('Spectra for (15 arcmin)^2 Pixel')
+            plt.savefig('absorber_power_law_single_map_RA'+str(j)+'_DEC'+str(k),dpi=300)
+            plt.clf()
+
+
+if filtered:
+    #Start with a single frequency band and 11 pixel filter
+#    for i in range(5,len(list_freq[0])-5):
+    full_filter = zeros((len(list_array),len(list_array[0]),16,10))
+    for f in range(0,len(list_array)):
+        for j in range(0,16):
+            for k in range(0,10):
+                for i in range(5,len(list_freq[0])-5):
+                    filter = list_array[f,i,j,k]-0.1*(list_array[f,i-5,j,k]+list_array[f,i-4,j,k]+list_array[f,i-3,j,k]+list_array[f,i-2,j,k]+list_array[f,i-1,j,k]+list_array[f,i+1,j,k]+list_array[f,i+2,j,k]+list_array[f,i+3,j,k]+list_array[f,i+4,j,k]+list_array[f,i+5,j,k])
+                    full_filter[f,i,j,k] = filter
+    mean_filter = ma.mean(full_filter, axis=1)
+    std_filter = ma.std(full_filter,axis=1)
+#    print mean_filter[0]
+#    print std_filter[0]
+    outliers = zeros((len(list_array),len(list_array[0]),16,10))
+    outliers2 = zeros((len(list_array),len(list_array[0]),16,10))
+    outliers3 = zeros((len(list_array),len(list_array[0]),16,10))
+    for f in range(0,len(list_array)):
+        for j in range(0,16):
+            for k in range(0,10):
+                for i in range(0,len(list_freq[0])):
+                    if full_filter[f,i,j,k]<(mean_filter[f,j,k]-std_filter[f,j,k]):
+                        outliers[f,i,j,k] = 1.0
+                    if full_filter[f,i,j,k]<(mean_filter[f,j,k]-2.*std_filter[f,j,k]):
+                        outliers2[f,i,j,k] = 1.0
+                    if full_filter[f,i,j,k]<(mean_filter[f,j,k]-3.*std_filter[f,j,k]):
+                        outliers3[f,i,j,k] = 1.0
+
+    if hit_plot:
+        for i in range(0,len(list_freq[0])):
+            for f in range(0,len(list_freq)):
+                plt.imshow(outliers[f,i])
+                plt.title('One Sigma Outliers for an 11 pixel filter')
+                plt.savefig('one_sigma_outliers_'+str(int(list_freq[f,i]*1000))+'kHz',dpi=300)
+                plt.clf()
+    
+    candidate_maps = []
+    candidate_freqs = []
+    candidate_ra = []
+    candidate_dec = []
+    for f in range(0,len(list_array)):
+        for i in range(0,len(list_freq[0])):
+            med_sum2 = ma.sum(outliers2[f,i,:,:],axis=0)
+            sum2 = ma.sum(med_sum2)
+            med_sum3 = ma.sum(outliers3[f,i,:,:],axis=0)
+            sum3 = ma.sum(med_sum3)
+            if sum3>=1.0:
+                if sum2<=2.0:
+                    candidate_maps.append(f)
+                    candidate_freqs.append(i)
+                    print where(outliers3[f,i]==1.0)
+                    candidate_ra.append(where(outliers3[f,i]==1.0)[0][0])
+                    candidate_dec.append(where(outliers3[f,i]==1.0)[1][0])
+                    if sum3==2.0:
+                        candidate_maps.append(f)
+                        candidate_freqs.append(i)
+                        candidate_ra.append(where(outliers3[f,i]==1.0)[0][1])
+                        candidate_dec.append(where(outliers3[f,i]==1.0)[1][1])
+                        
+    print len(candidate_maps)
+
+    if candidate_plot:
+        for g in range(0,len(candidate_maps)):
+            min_freq = candidate_freqs[g]-30
+            max_freq = candidate_freqs[g]+30
+            if candidate_freqs[g]<30:
+                min_freq = 0
+            elif candidate_freqs[g]>378:
+                max_freq = -1
+            mean_signal = ma.mean(list_array[candidate_maps[g],min_freq:max_freq,candidate_ra[g],candidate_dec[g]])
+            plt.plot(1000*list_freq[candidate_maps[g],min_freq:max_freq],list_array[candidate_maps[g],min_freq:max_freq,candidate_ra[g],candidate_dec[g]]-mean_signal)
+            plt.xlabel('Frequency (kHz)')
+            plt.axvline(1000*list_freq[candidate_maps[g],candidate_freqs[g]],c='r',ls='--')
+            plt.ylabel('Temperature (Kelvin)')
+            plt.grid()
+            plt.title('Mean Subtracted Absorber Candidate Spectra at RA %0.1f, DEC %0.1f' %(ras[candidate_ra[g]],decs[candidate_dec[g]]))
+            plt.savefig('absorber_candidate_at_'+str(int(list_freq[candidate_maps[g],candidate_freqs[g]]*1000))+'kHz',dpi=300)
+            plt.clf()
+
+#plt.subplot(121)
+#plt.imshow(list_array[:,:,8,5],aspect=len(list_array[0])/len(list_array),vmin=-0.6,vmax=0.6,interpolation='nearest')
+#plt.colorbar()
+#plt.title('Before Subtraction')
+#plt.subplot(122)
+#plt.imshow(list_array[:,:,8,5]-fit_data[:,:,8,5],aspect=len(list_array[0])/len(list_array),vmin=-0.1,vmax=0.1,interpolation='nearest')
+#plt.title('After Subtraction')
+#plt.colorbar()
 #plt.show()
-fit_data = zeros((2000,16))
-for i in range(0,16):
-    fit_data[:,i] = function(list_freq,list_array[:,i,5])
-
-plt.subplot(121)
-plt.imshow(list_array[:,:,5],aspect=16.0/2000,vmin=-0.6,vmax=0.6,interpolation='nearest')
-plt.colorbar()
-plt.title('Before Subtraction')
-plt.subplot(122)
-plt.imshow(list_array[:,:,5]-fit_data,aspect=16.0/2000,vmin=-0.1,vmax=0.1,interpolation='nearest')
-plt.title('After Subtraction')
-plt.colorbar()
-plt.show()
 
 #for slice, freq in enumerate(freqs):
 #   nancut = (array[slice] < 10e10) & ( array[slice] != NaN )
